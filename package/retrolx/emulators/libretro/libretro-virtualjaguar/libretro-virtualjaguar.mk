6266fdb2199865919ce570b532f2e5cfd88a4295
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-12 22:20:20 +0200

    Move lr-virtualjaguar to package system

---
+++ package/retrolx/emulators/libretro/libretro-virtualjaguar/libretro-virtualjaguar.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# VIRTUALJAGUAR
+#
+################################################################################
+# Version.: Commits on Mar 14, 2021
+LIBRETRO_VIRTUALJAGUAR_VERSION = 2069160f316d09a2713286bd9bf4d5c2805bd143
+LIBRETRO_VIRTUALJAGUAR_SITE = $(call github,libretro,virtualjaguar-libretro,$(LIBRETRO_VIRTUALJAGUAR_VERSION))
+LIBRETRO_VIRTUALJAGUAR_LICENSE = GPLv3
+
+LIBRETRO_VIRTUALJAGUAR_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_VIRTUALJAGUAR_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-virtualjaguar
+
+define LIBRETRO_VIRTUALJAGUAR_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)
+endef
+
+define LIBRETRO_VIRTUALJAGUAR_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_VIRTUALJAGUAR_PKG_DIR)$(LIBRETRO_VIRTUALJAGUAR_PKG_INSTALL_DIR)
+
+	# Copy package files
+	$(INSTALL) -D $(@D)/virtualjaguar_libretro.so \
+	$(LIBRETRO_VIRTUALJAGUAR_PKG_DIR)$(LIBRETRO_VIRTUALJAGUAR_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_VIRTUALJAGUAR_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-virtualjaguar/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
+endef
+
+LIBRETRO_VIRTUALJAGUAR_POST_INSTALL_TARGET_HOOKS = LIBRETRO_VIRTUALJAGUAR_MAKEPKG
+
+$(eval $(generic-package))
