e7a963a6c94ce26bb7714a064633a8d268e31dde
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-19 23:23:43 +0200

    Move lr-fceumm + lr-nestopia to package system

--- package/retrolx/emulators/libretro/libretro-fceumm/libretro-fceumm.mk
+++ package/retrolx/emulators/libretro/libretro-fceumm/libretro-fceumm.mk
@@ -7,6 +7,9 @@ LIBRETRO_FCEUMM_VERSION = a42bd198a0c28f367a7ef255ba63cf2827eaa3b0
 LIBRETRO_FCEUMM_SITE = $(call github,libretro,libretro-fceumm,$(LIBRETRO_FCEUMM_VERSION))
 LIBRETRO_FCEUMM_LICENSE = GPLv2
 
+LIBRETRO_FCEUMM_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_FCEUMM_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-fceumm
+
 LIBRETRO_FCEUMM_PLATFORM = $(LIBRETRO_PLATFORM)
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
@@ -27,9 +30,24 @@ define LIBRETRO_FCEUMM_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_FCEUMM_PLATFORM)"
 endef
 
-define LIBRETRO_FCEUMM_INSTALL_TARGET_CMDS
+define LIBRETRO_FCEUMM_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_FCEUMM_PKG_DIR)$(LIBRETRO_FCEUMM_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/fceumm_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/fceumm_libretro.so
+	$(LIBRETRO_FCEUMM_PKG_DIR)$(LIBRETRO_FCEUMM_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_FCEUMM_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-fceumm/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_FCEUMM_POST_INSTALL_TARGET_HOOKS = LIBRETRO_FCEUMM_MAKEPKG
 
 $(eval $(generic-package))
--- package/retrolx/emulators/libretro/libretro-nestopia/libretro-nestopia.mk
+++ package/retrolx/emulators/libretro/libretro-nestopia/libretro-nestopia.mk
@@ -7,6 +7,9 @@ LIBRETRO_NESTOPIA_VERSION = c924b9d2cf8918b42fa1fa7db073a07a04e0c8cc
 LIBRETRO_NESTOPIA_SITE = $(call github,libretro,nestopia,$(LIBRETRO_NESTOPIA_VERSION))
 LIBRETRO_NESTOPIA_LICENSE = GPLv2
 
+LIBRETRO_NESTOPIA_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_NESTOPIA_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-nestopia
+
 LIBRETRO_NESTOPIA_PLATFORM = $(LIBRETRO_PLATFORM)
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S812),y)
@@ -26,14 +29,26 @@ define LIBRETRO_NESTOPIA_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro/ platform="$(LIBRETRO_NESTOPIA_PLATFORM)"
 endef
 
-define LIBRETRO_NESTOPIA_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/libretro/nestopia_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/nestopia_libretro.so
+define LIBRETRO_NESTOPIA_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_BSNES_PKG_DIR)$(LIBRETRO_NESTOPIA_PKG_INSTALL_DIR)/bios
 
-	# Bios
-	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/bios
+	# Copy package files
+	$(INSTALL) -D $(@D)/libretro/nestopia_libretro.so \
+	$(LIBRETRO_NESTOPIA_PKG_DIR)$(LIBRETRO_NESTOPIA_PKG_INSTALL_DIR)
 	$(INSTALL) -D $(@D)/NstDatabase.xml \
-		$(TARGET_DIR)/usr/share/batocera/datainit/bios/NstDatabase.xml
+	$(LIBRETRO_NESTOPIA_PKG_DIR)$(LIBRETRO_NESTOPIA_PKG_INSTALL_DIR)/bios/NstDatabase.xml
+
+	# Build Pacman package
+	cd $(LIBRETRO_NESTOPIA_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-bsnes/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_NESTOPIA_POST_INSTALL_TARGET_HOOKS = LIBRETRO_NESTOPIA_MAKEPKG
 
 $(eval $(generic-package))
