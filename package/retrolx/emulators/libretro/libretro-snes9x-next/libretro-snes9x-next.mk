2ff1967f4c98fa71eabd0a4710ca6940ea3ab9ba
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-20 09:00:13 +0200

    Move lr-snes9x and lr-snes9x-next to package system

--- package/retrolx/emulators/libretro/libretro-snes9x-next/libretro-snes9x-next.mk
+++ package/retrolx/emulators/libretro/libretro-snes9x-next/libretro-snes9x-next.mk
@@ -8,6 +8,9 @@ LIBRETRO_SNES9X_NEXT_VERSION = 1649a31d1ea0cc10a181ea4fec2698950c4ee780
 LIBRETRO_SNES9X_NEXT_SITE = $(call github,libretro,snes9x2010,$(LIBRETRO_SNES9X_NEXT_VERSION))
 LIBRETRO_SNES9X_NEXT_LICENSE = Non-commercial
 
+LIBRETRO_SNES9X_NEXT_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_SNES9X_NEXT_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-snes9x-next
+
 LIBRETRO_SNES9X_NEXT_PLATFORM = $(LIBRETRO_PLATFORM)
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S922X),y)
@@ -34,9 +37,24 @@ define LIBRETRO_SNES9X_NEXT_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_SNES9X_NEXT_PLATFORM)"
 endef
 
-define LIBRETRO_SNES9X_NEXT_INSTALL_TARGET_CMDS
+define LIBRETRO_SNES9X_NEXT_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_SNES9X_NEXT_PKG_DIR)$(LIBRETRO_SNES9X_NEXT_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/snes9x2010_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/snes9x_next_libretro.so
+	$(LIBRETRO_SNES9X_NEXT_PKG_DIR)$(LIBRETRO_SNES9X_NEXT_PKG_INSTALL_DIR)/snes9x_next_libretro.so
+
+	# Build Pacman package
+	cd $(LIBRETRO_SNES9X_NEXT_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-snes9x-next/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_SNES9X_NEXT_POST_INSTALL_TARGET_HOOKS = LIBRETRO_SNES9X_NEXT_MAKEPKG
 
 $(eval $(generic-package))
--- package/retrolx/emulators/libretro/libretro-snes9x/libretro-snes9x.mk
+++ package/retrolx/emulators/libretro/libretro-snes9x/libretro-snes9x.mk
@@ -8,6 +8,9 @@ LIBRETRO_SNES9X_VERSION = 937c177ef5c8a5d8e535d00d28acf7125593b4ba
 LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
 LIBRETRO_SNES9X_LICENSE = Non-commercial
 
+LIBRETRO_SNES9X_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_SNES9X_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-snes9x
+
 LIBRETRO_SNES9X_PLATFORM = $(LIBRETRO_PLATFORM)
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S922X),y)
@@ -20,9 +23,24 @@ define LIBRETRO_SNES9X_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro -f Makefile platform="$(LIBRETRO_SNES9X_PLATFORM)"
 endef
 
-define LIBRETRO_SNES9X_INSTALL_TARGET_CMDS
+define LIBRETRO_SNES9X_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_SNES9X_PKG_DIR)$(LIBRETRO_SNES9X_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/libretro/snes9x_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/snes9x_libretro.so
+	$(LIBRETRO_SNES9X_PKG_DIR)$(LIBRETRO_SNES9X_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_SNES9X_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-snes9x/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_SNES9X_POST_INSTALL_TARGET_HOOKS = LIBRETRO_SNES9X_MAKEPKG
 
 $(eval $(generic-package))
