b5be2554b61a6a84ec27d1eb57e545f70e0865fd
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-23 11:56:46 +0200

    Fix retrolx-rebuild-es-systems.sh script

--- package/retrolx/frontends/emulationstation/retrolx-es-system/retrolx-rebuild-es-systems.sh
+++ package/retrolx/frontends/emulationstation/retrolx-es-system/retrolx-rebuild-es-systems.sh
@@ -4,10 +4,10 @@
 /usr/share/emulationstation/es_systems.yml \
 /usr/share/emulationstation/es_features.yml \
 /userdata/system/.config \
-/userdata/system/es_systems.cfg \
-/userdata/system/es_features.cfg \
+/userdata/system/configs/emulationstation/es_systems.cfg \
+/userdata/system/configs/emulationstation/es_features.cfg \
 /usr/share/batocera/configgen/configgen-defaults.yml \
 /usr/share/batocera/configgen/configgen-defaults-arch.yml \
 /usr/share/emulationstation/roms \
-/userdata/roms $(BATOCERA_SYSTEM_ARCH)
+/userdata/roms {BATOCERA_ARCHITECTURE}
 
