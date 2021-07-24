55523b66b81dd69aed4ee4db47331b730ed93ee1
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-24 02:22:04 +0200

    lr-gpsp and lr-prosystem

--- package/retrolx/emulators/libretro/libretro-gpsp/libretro-gpsp.mk
+++ package/retrolx/emulators/libretro/libretro-gpsp/libretro-gpsp.mk
@@ -3,17 +3,35 @@
 # GPSP
 #
 ################################################################################
-LIBRETRO_GPSP_VERSION = 0ca87a4807b4d26baef3b7813e1b948b88dddf79
+LIBRETRO_GPSP_VERSION = d468f473e84a8fc36a68b6d1da9b422e52852820
 LIBRETRO_GPSP_SITE = $(call github,libretro,gpsp,$(LIBRETRO_GPSP_VERSION))
 LIBRETRO_GPSP_LICENSE = GPLv2
+
+LIBRETRO_GPSP_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_GPSP_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-gpsp
 
 define LIBRETRO_GPSP_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform=unix
 endef
 
-define LIBRETRO_GPSP_INSTALL_TARGET_CMDS
+define LIBRETRO_GPSP_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_GPSP_PKG_DIR)$(LIBRETRO_GPSP_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/gpsp_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/gpsp_libretro.so
+	$(LIBRETRO_GPSP_PKG_DIR)$(LIBRETRO_GPSP_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_GPSP_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-gpsp/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_GPSP_POST_INSTALL_TARGET_HOOKS = LIBRETRO_GPSP_MAKEPKG
 
 $(eval $(generic-package))
--- package/retrolx/emulators/libretro/libretro-prosystem/libretro-prosystem.mk
+++ package/retrolx/emulators/libretro/libretro-prosystem/libretro-prosystem.mk
@@ -3,10 +3,13 @@
 # PROSYSTEM
 #
 ################################################################################
-# Version.: Commits on May 12, 2021
-LIBRETRO_PROSYSTEM_VERSION = fa44adacf8af44a86c94462f90829bb9f8cd8a11
+# Version.: Commits on Jul 12, 2021
+LIBRETRO_PROSYSTEM_VERSION = 30bd65a64fa28623155a94cbbba2444e228157b7
 LIBRETRO_PROSYSTEM_SITE = $(call github,libretro,prosystem-libretro,$(LIBRETRO_PROSYSTEM_VERSION))
 LIBRETRO_PROSYSTEM_LICENSE = GPLv2
+
+LIBRETRO_PROSYSTEM_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_PROSYSTEM_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-prosystem
 
 LIBRETRO_PROSYSTEM_PLATFORM = $(LIBRETRO_PLATFORM)
 
@@ -27,9 +30,24 @@ define LIBRETRO_PROSYSTEM_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PROSYSTEM_PLATFORM)"
 endef
 
-define LIBRETRO_PROSYSTEM_INSTALL_TARGET_CMDS
+define LIBRETRO_PROSYSTEM_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_PROSYSTEM_PKG_DIR)$(LIBRETRO_PROSYSTEM_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/prosystem_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/prosystem_libretro.so
+	$(LIBRETRO_PROSYSTEM_PKG_DIR)$(LIBRETRO_PROSYSTEM_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_PROSYSTEM_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-prosystem/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_PROSYSTEM_POST_INSTALL_TARGET_HOOKS = LIBRETRO_PROSYSTEM_MAKEPKG
 
 $(eval $(generic-package))
