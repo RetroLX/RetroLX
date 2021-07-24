dbcae9a716743aeb15a3312204d6391d3d3a4137
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-24 02:27:51 +0200

    lr-vba-m

--- package/retrolx/emulators/libretro/libretro-vba-m/libretro-vba-m.mk
+++ package/retrolx/emulators/libretro/libretro-vba-m/libretro-vba-m.mk
@@ -3,16 +3,35 @@
 # VBA-M
 #
 ################################################################################
-LIBRETRO_VBA_M_VERSION = c08bb6bdb0c8576ea60aa41a22f96eaaeac6adea
+# Version.: Commits on Jul 23, 2021
+LIBRETRO_VBA_M_VERSION = 96ce316bcd159253e9f724a807fb23af5f51706c
 LIBRETRO_VBA_M_SITE = $(call github,visualboyadvance-m,visualboyadvance-m,$(LIBRETRO_VBA_M_VERSION))
+
+LIBRETRO_VBA_M_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_VBA_M_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-vba-m
 
 define LIBRETRO_VBA_M_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/src/libretro -f Makefile platform="unix"
 endef
 
-define LIBRETRO_VBA_M_INSTALL_TARGET_CMDS
+define LIBRETRO_VBA_M_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_VBA_M_PKG_DIR)$(LIBRETRO_VBA_M_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/src/libretro/vbam_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/vba-m_libretro.so
+	$(LIBRETRO_VBA_M_PKG_DIR)$(LIBRETRO_VBA_M_PKG_INSTALL_DIR)/vba-m_libretro.so
+
+	# Build Pacman package
+	cd $(LIBRETRO_VBA_M_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-vba-m/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_VBA_M_POST_INSTALL_TARGET_HOOKS = LIBRETRO_VBA_M_MAKEPKG
 
 $(eval $(generic-package))
