203b31eae833ed9f4e8ce2aa2d40ac0adb262374
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-26 00:15:39 +0200

    lr-fmsx

--- package/retrolx/emulators/libretro/libretro-fmsx/libretro-fmsx.mk
+++ package/retrolx/emulators/libretro/libretro-fmsx/libretro-fmsx.mk
@@ -8,6 +8,9 @@ LIBRETRO_FMSX_VERSION = d0581d40a40b231d619d2c5363fc2e0ecefeafbd
 LIBRETRO_FMSX_SITE = $(call github,libretro,fmsx-libretro,$(LIBRETRO_FMSX_VERSION))
 LIBRETRO_FMSX_LICENSE = GPLv2
 
+LIBRETRO_FMSX_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_FMSX_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-fmsx
+
 LIBRETRO_FMSX_PLATFORM = $(LIBRETRO_PLATFORM)
 LIBRETRO_FMSX_EXTRA_ARGS =
 
@@ -26,9 +29,24 @@ define LIBRETRO_FMSX_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_FMSX_PLATFORM)" $(LIBRETRO_FMSX_EXTRA_ARGS)
 endef
 
-define LIBRETRO_FMSX_INSTALL_TARGET_CMDS
+define LIBRETRO_FMSX_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_FMSX_PKG_DIR)$(LIBRETRO_FMSX_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/fmsx_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/fmsx_libretro.so
+	$(LIBRETRO_FMSX_PKG_DIR)$(LIBRETRO_FMSX_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_FMSX_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-fmsx/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_FMSX_POST_INSTALL_TARGET_HOOKS = LIBRETRO_FMSX_MAKEPKG
 
 $(eval $(generic-package))
