80f66a20c2a1871460b784a35178592dc2c3c29e
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-28 20:18:20 +0200

    Move RK3288 boards to U-Boot 2021.07

--- board/batocera/rockchip/rk3288/tinkerboard/build-uboot.sh
+++ board/batocera/rockchip/rk3288/tinkerboard/build-uboot.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-git clone --depth 1 https://source.denx.de/u-boot/u-boot.git -b v2019.04
+git clone --depth 1 https://source.denx.de/u-boot/u-boot.git -b v2021.07
 cd u-boot
-make tinker-rk3288_defconfig
-ARCH=arm CROSS_COMPILE=/h3/host/bin/arm-buildroot-linux-gnueabihf- make -j$(nproc)
+ARCH=arm CROSS_COMPILE=/rk3288/host/bin/arm-buildroot-linux-gnueabihf- make -j$(nproc) tinker-rk3288_defconfig all
 mkdir -p ../../uboot-tinkerboard
-cp u-boot-sunxi-with-spl.bin ../../uboot-tinkerboard/
-"${HOST_DIR}/bin/mkimage" -n rk3288 -T rksd -d "${BINARIES_DIR}/tinkerboard/u-boot-spl-dtb.bin" "${BINARIES_DIR}/tinkerboard/u-boot-spl-dtb.img" || exit 1
-cat "$BINARIES_DIR/tinkerboard/u-boot-dtb.bin" >> "${BINARIES_DIR}/tinkerboard/u-boot-spl-dtb.img" || exit 1
+cp u-boot-dtb.img ../../uboot-tinkerboard/
+"/rk3288/host/bin/mkimage" -n rk3288 -T rksd -d "tpl/u-boot-tpl.bin" "../../uboot-tinkerboard/u-boot-tpl.img" || exit 1
+cat "spl/u-boot-spl-dtb.bin" >> "../../uboot-tinkerboard/u-boot-tpl.img" || exit 1
 
