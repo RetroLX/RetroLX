23b1a5de81f7baa6b0835bef2a6160342096f83b
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-28 16:24:45 +0200

    lr-freeintv

--- package/retrolx/emulators/libretro/libretro-freeintv/libretro-freeintv.mk
+++ package/retrolx/emulators/libretro/libretro-freeintv/libretro-freeintv.mk
@@ -8,6 +8,9 @@ LIBRETRO_FREEINTV_VERSION = 5fc8d85ee9699baaaf0c63399c364f456097fc1e
 LIBRETRO_FREEINTV_SITE = $(call github,libretro,freeintv,$(LIBRETRO_FREEINTV_VERSION))
 LIBRETRO_FREEINTV_LICENSE = GPLv3
 
+LIBRETRO_FREEINTV_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_FREEINTV_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-freeintv
+
 define LIBRETRO_FREEINTV_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile platform="unix"
 endef
@@ -16,5 +19,25 @@ define LIBRETRO_FREEINTV_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/freeintv_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/freeintv_libretro.so
 endef
+
+define LIBRETRO_FREEINTV_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_FREEINTV_PKG_DIR)$(LIBRETRO_FREEINTV_PKG_INSTALL_DIR)
+
+	# Copy package files
+	$(INSTALL) -D $(@D)/prboom_libretro.so \
+	$(LIBRETRO_FREEINTV_PKG_DIR)$(LIBRETRO_FREEINTV_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_FREEINTV_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-freeintv/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
+endef
+
+LIBRETRO_FREEINTV_POST_INSTALL_TARGET_HOOKS = LIBRETRO_FREEINTV_MAKEPKG
 
 $(eval $(generic-package))
