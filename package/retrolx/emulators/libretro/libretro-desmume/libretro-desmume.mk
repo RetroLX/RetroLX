123202116047e5f9ced9a599808cf1484fed36ae
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-30 23:35:47 +0200

    lr-desmume

--- package/retrolx/emulators/libretro/libretro-desmume/libretro-desmume.mk
+++ package/retrolx/emulators/libretro/libretro-desmume/libretro-desmume.mk
@@ -9,13 +9,31 @@ LIBRETRO_DESMUME_SITE = $(call github,libretro,desmume,$(LIBRETRO_DESMUME_VERSIO
 LIBRETRO_DESMUME_LICENSE = GPLv2
 LIBRETRO_DESMUME_DEPENDENCIES = libpcap
 
+LIBRETRO_DESMUME_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_DESMUME_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-desmume
+
 define LIBRETRO_DESMUME_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/desmume/src/frontend/libretro -f Makefile platform="$(LIBRETRO_PLATFORM)"
 endef
 
-define LIBRETRO_DESMUME_INSTALL_TARGET_CMDS
+define LIBRETRO_DESMUME_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_DESMUME_PKG_DIR)$(LIBRETRO_DESMUME_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/desmume/src/frontend/libretro/desmume_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/desmume_libretro.so
+	$(LIBRETRO_DESMUME_PKG_DIR)$(LIBRETRO_DESMUME_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_DESMUME_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-desmume/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_DESMUME_POST_INSTALL_TARGET_HOOKS = LIBRETRO_DESMUME_MAKEPKG
 
 $(eval $(generic-package))
