c2f9d58efb2572f011199a42ce67c8017587636c
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-30 23:47:11 +0200

    lr-ecwolf

--- package/retrolx/emulators/libretro/libretro-ecwolf/libretro-ecwolf.mk
+++ package/retrolx/emulators/libretro/libretro-ecwolf/libretro-ecwolf.mk
@@ -8,15 +8,33 @@ LIBRETRO_ECWOLF_VERSION = a2c0d7bd158ef086adcd78d498df19233d5ba0c7
 LIBRETRO_ECWOLF_SITE = $(call github,libretro,ecwolf,$(LIBRETRO_ECWOLF_VERSION))
 LIBRETRO_ECWOLF_LICENSE = Non-commercial
 
+LIBRETRO_ECWOLF_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_ECWOLF_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-ecwolf
+
 LIBRETRO_ECWOLF_PLATFORM = $(LIBRETRO_PLATFORM)
 
 define LIBRETRO_ECWOLF_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" GIT_VERSION="" -C $(@D)/src/libretro/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
 endef
 
-define LIBRETRO_ECWOLF_INSTALL_TARGET_CMDS
+define LIBRETRO_ECWOLF_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_ECWOLF_PKG_DIR)$(LIBRETRO_ECWOLF_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/src/libretro/ecwolf_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/ecwolf_libretro.so
+	$(LIBRETRO_ECWOLF_PKG_DIR)$(LIBRETRO_ECWOLF_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_ECWOLF_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-ecwolf/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_ECWOLF_POST_INSTALL_TARGET_HOOKS = LIBRETRO_ECWOLF_MAKEPKG
 
 $(eval $(generic-package))
