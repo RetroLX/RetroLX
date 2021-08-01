822ce1eb4732e3704c1a128d7cae6601217b6941
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-01 22:31:04 +0200

    Bump FAudio to 21.08 + bump xow (gcc 11)

--- package/batocera/utils/xow/xow.mk
+++ package/batocera/utils/xow/xow.mk
@@ -3,7 +3,7 @@
 # xow
 #
 ################################################################################
-XOW_VERSION = cd271644ead198dcbfeafd3c3411092850adfd3a
+XOW_VERSION = 700529b2517df7d17ad5a2fa0bd679143ac48666
 XOW_SITE = $(call github,medusalix,xow,$(XOW_VERSION))
 XOW_DEPENDENCIES = host-libcurl host-cabextract libusb
 
