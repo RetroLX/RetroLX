004863a9cbabe246b649dfc8cdc54e20ba4c36ce
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-01 20:39:32 +0200

    WIP RPCS3 bump to 0.0.17, not compiling due to glibc 2.32 / 2.33 conflict in llvm-tblgen

--- package/batocera/emulators/rpcs3/rpcs3.mk
+++ package/batocera/emulators/rpcs3/rpcs3.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# 2021 May, 1 release
-RPCS3_VERSION = v0.0.16
+# 2021 July, 1 release
+RPCS3_VERSION = v0.0.17
 
 RPCS3_SITE = https://github.com/RPCS3/rpcs3.git
 RPCS3_SITE_METHOD=git
@@ -18,7 +18,7 @@ RPCS3_CONF_OPTS += -DUSE_SYSTEM_FFMPEG=ON
 RPCS3_CONF_OPTS += -DUSE_SYSTEM_LIBPNG=ON
 RPCS3_CONF_OPTS += -DUSE_DISCORD_RPC=OFF
 RPCS3_CONF_OPTS += -DUSE_VULKAN=ON
-#RPCS3_CONF_OPTS += -DCMAKE_CROSSCOMPILING=ON
+RPCS3_CONF_OPTS += -DCMAKE_CROSSCOMPILING=ON
 RPCS3_CONF_OPTS += -DWITH_LLVM=ON
 RPCS3_CONF_OPTS += -DBUILD_LLVM_SUBMODULE=ON
 RPCS3_CONF_OPTS += -DUSE_NATIVE_INSTRUCTIONS=OFF
@@ -26,12 +26,13 @@ RPCS3_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 RPCS3_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
 RPCS3_CONF_ENV += PATH="/x86_64/host/x86_64-buildroot-linux-gnu/sysroot/usr/bin:$$PATH"
+RPCS3_CONF_ENV += LD_LIBRARY_PATH="/x86_64/host/lib:/x86_64/host/usr/lib:$$LD_LIBRARY_PATH"
 
 define RPCS3_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
 		CC_FOR_BUILD="$(TARGET_CC)" GCC_FOR_BUILD="$(TARGET_CC)" \
 		CXX_FOR_BUILD="$(TARGET_CXX)" LD_FOR_BUILD="$(TARGET_LD)" \
-                CROSS_COMPILE="$(STAGING_DIR)/usr/bin/" \
+                CROSS_COMPILE="$(STAGING_DIR)/usr/bin/" LD_LIBRARY_PATH="$(HOST_DIR)/usr/lib:$(HOST_DIR)/lib:$(LD_LIBRARY_PATH)" \
                 PREFIX="/x86_64/host/x86_64-buildroot-linux-gnu/sysroot/" \
                 PKG_CONFIG="/x86_64/host/x86_64-buildroot-linux-gnu/sysroot/usr/bin/pkg-config" \
 		$(MAKE) -C $(@D)
