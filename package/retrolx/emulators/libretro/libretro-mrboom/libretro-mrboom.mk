e511d2befe8a5d8633c26c2fe4c4cc6f95fd9611
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-03 01:15:59 +0200

    lr-mrboom

--- package/retrolx/emulators/libretro/libretro-mrboom/libretro-mrboom.mk
+++ package/retrolx/emulators/libretro/libretro-mrboom/libretro-mrboom.mk
@@ -10,6 +10,9 @@ LIBRETRO_MRBOOM_SITE_METHOD=git
 LIBRETRO_MRBOOM_GIT_SUBMODULES=YES
 LIBRETRO_MRBOOM_LICENSE=GPLv2
 
+LIBRETRO_MRBOOM_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_MRBOOM_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-mrboom
+
 ifeq ($(BR2_ARM_FPU_NEON_VFPV4)$(BR2_ARM_FPU_NEON)$(BR2_ARM_FPU_NEON_FP_ARMV8),y)
 LIBRETRO_MRBOOM_EXTRA_ARGS = HAVE_NEON=1
 endif
@@ -18,9 +21,24 @@ define LIBRETRO_MRBOOM_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform=unix $(LIBRETRO_MRBOOM_EXTRA_ARGS)
 endef
 
-define LIBRETRO_MRBOOM_INSTALL_TARGET_CMDS
+define LIBRETRO_MRBOOM_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_MRBOOM_PKG_DIR)$(LIBRETRO_MRBOOM_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/mrboom_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/mrboom_libretro.so
+	$(LIBRETRO_MRBOOM_PKG_DIR)$(LIBRETRO_MRBOOM_PKG_INSTALL_DIR)/mrboom_libretro.so
+
+	# Build Pacman package
+	cd $(LIBRETRO_MRBOOM_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-mrboom/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_MRBOOM_POST_INSTALL_TARGET_HOOKS = LIBRETRO_MRBOOM_MAKEPKG
 
 $(eval $(generic-package))
