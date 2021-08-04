bc70b52a254e00c46ccff891e1a3de2398d93ad6
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-04 23:02:12 +0200

    lr-fuse

--- package/retrolx/emulators/libretro/libretro-fuse/libretro-fuse.mk
+++ package/retrolx/emulators/libretro/libretro-fuse/libretro-fuse.mk
@@ -3,17 +3,36 @@
 # FUSE
 #
 ################################################################################
-LIBRETRO_FUSE_VERSION = 5b1c05330e907556d99a8caca769ce273fdc9198
+# Version.: Commits on Aug 3, 2021
+LIBRETRO_FUSE_VERSION = 5f331e9772d305ba5209db0910b1963b9d0974c0
 LIBRETRO_FUSE_SITE = $(call github,libretro,fuse-libretro,$(LIBRETRO_FUSE_VERSION))
 LIBRETRO_FUSE_LICENSE = GPLv3
+
+LIBRETRO_FUSE_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_FUSE_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-fuse
 
 define LIBRETRO_FUSE_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
 endef
 
-define LIBRETRO_FUSE_INSTALL_TARGET_CMDS
+define LIBRETRO_FUSE_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_FUSE_PKG_DIR)$(LIBRETRO_FUSE_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/fuse_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/fuse_libretro.so
+	$(LIBRETRO_FUSE_PKG_DIR)$(LIBRETRO_FUSE_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_FUSE_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-fuse/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_FUSE_POST_INSTALL_TARGET_HOOKS = LIBRETRO_FUSE_MAKEPKG
 
 $(eval $(generic-package))
