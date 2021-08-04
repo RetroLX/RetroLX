d5d4e984774e2d524f4bb40434f5638562a899b6
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-04 23:07:35 +0200

    lr-gw

--- package/retrolx/emulators/libretro/libretro-gw/libretro-gw.mk
+++ package/retrolx/emulators/libretro/libretro-gw/libretro-gw.mk
@@ -3,18 +3,36 @@
 # GW
 #
 ################################################################################
-# Version.: Commits on Apr 25, 2021
-LIBRETRO_GW_VERSION = 50538984da8755db55577683cc98912e26ee8d8a
+# Version.: Commits on Aug 2, 2021
+LIBRETRO_GW_VERSION = 7b3e374a1067a80324f1e4905136bae357ec18da
 LIBRETRO_GW_SITE = $(call github,libretro,gw-libretro,$(LIBRETRO_GW_VERSION))
 LIBRETRO_GW_LICENSE = GPLv3
+
+LIBRETRO_GW_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_GW_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-gw
 
 define LIBRETRO_GW_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
 endef
 
-define LIBRETRO_GW_INSTALL_TARGET_CMDS
+define LIBRETRO_GW_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_GW_PKG_DIR)$(LIBRETRO_GW_PKG_INSTALL_DIR)
+
+	# Copy package files
 	$(INSTALL) -D $(@D)/gw_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/gw_libretro.so
+	$(LIBRETRO_GW_PKG_DIR)$(LIBRETRO_GW_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_GW_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-gw/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+LIBRETRO_GW_POST_INSTALL_TARGET_HOOKS = LIBRETRO_GW_MAKEPKG
 
 $(eval $(generic-package))
