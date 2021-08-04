d0cb1416f49aa7393f577bafd78ed50ac8461746
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-04 15:10:15 +0200

    Rework pacman / ES integration

--- package/batocera/utils/pacman/pacman.mk
+++ package/batocera/utils/pacman/pacman.mk
@@ -23,8 +23,10 @@ define BATOCERA_PACMAN_INSTALL_CONF
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/pacman/batocera-uninstall.hook $(TARGET_DIR)/etc/pacman/hooks/batocera/batocera-uninstall.hook
 
 	mkdir -p $(TARGET_DIR)/etc/pacman/hooks/retrolx
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/pacman/retrolx-install.hook $(TARGET_DIR)/etc/pacman/hooks/retrolx/retrolx-install-es-system.hook
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/pacman/retrolx-uninstall.hook $(TARGET_DIR)/etc/pacman/hooks/retrolx/retrolx-uninstall-es-system.hook
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/pacman/retrolx-install-es-system.hook \
+	$(TARGET_DIR)/etc/pacman/hooks/retrolx/retrolx-install-es-system.hook
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/pacman/retrolx-uninstall-es-system.hook \
+	$(TARGET_DIR)/etc/pacman/hooks/retrolx/retrolx-uninstall-es-system.hook
 
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/pacman/userdata_pacman.conf $(TARGET_DIR)/usr/share/batocera/datainit/system/pacman/pacman.conf
 	sed -i -e s+"{BATOCERA_ARCHITECTURE}"+"$(BATOCERA_SYSTEM_ARCH)"+ $(TARGET_DIR)/etc/batocera_pacman.conf
