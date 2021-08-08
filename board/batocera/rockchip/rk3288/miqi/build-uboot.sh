1ae1f83b636d3b5ccf984db2242ca0828f4861fa
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-08 23:38:23 +0200

    Fix h5 atf25 and miqi boot

--- board/batocera/rockchip/rk3288/miqi/build-uboot.sh
+++ board/batocera/rockchip/rk3288/miqi/build-uboot.sh
@@ -5,6 +5,5 @@ cd u-boot
 ARCH=arm CROSS_COMPILE=/rk3288/host/bin/arm-buildroot-linux-gnueabihf- make -j$(nproc) miqi-rk3288_defconfig all
 mkdir -p ../../uboot-miqi
 cp u-boot-dtb.img ../../uboot-miqi/
-"/rk3288/host/bin/mkimage" -n rk3288 -T rksd -d "tpl/u-boot-tpl.bin" "../../uboot-miqi/u-boot-tpl.img" || exit 1
-cat "spl/u-boot-spl-dtb.bin" >> "../../uboot-miqi/u-boot-tpl.img" || exit 1
-
+"/rk3288/host/bin/mkimage" -n rk3288 -T rksd -d "spl/u-boot-spl-dtb.bin" "../../uboot-miqi/u-boot-tpl.img" || exit 1
+cat "u-boot-dtb.bin" >> "../../uboot-miqi/u-boot-tpl.img" || exit 1
