6e02963d368a10a6a1868dbdd62b61e265ce468c
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-10 08:35:08 +0200

    Post build H5 uboot

--- board/batocera/allwinner/h5/tritium-h5/build-uboot.sh
+++ board/batocera/allwinner/h5/tritium-h5/build-uboot.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# ARM Trusted Firmware BL31
+export BL31="/h5/images/bl31.bin"
+# Crust firmware (optional)
+export SCP="/dev/null"
+
+# Clone U-Boot mainline
 git clone --depth 1 https://source.denx.de/u-boot/u-boot.git -b v2021.07
 cd u-boot
+
+# Make config
 make libretech_all_h3_cc_h5_defconfig
-ARCH=aarch64 CROSS_COMPILE=/h5/host/aarch64-buildroot-linux-gnu- make -j$(nproc)
+
+# Build it
+ARCH=aarch64 CROSS_COMPILE=/h5/host/bin/aarch64-buildroot-linux-gnu- make -j$(nproc)
 mkdir -p ../../uboot-tritium-h5
+
+# Copy to appropriate place
 cp u-boot-sunxi-with-spl.bin ../../uboot-tritium-h5/
