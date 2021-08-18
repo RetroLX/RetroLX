0a5a63e39b579f36725e8426d8ae31e0d9c446af
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-19 00:26:12 +0200

    externalGenerator core loader OK

--- package/retrolx/core/retrolx-configgen/configgen/configgen/generators/external/externalGenerator.py
+++ package/retrolx/core/retrolx-configgen/configgen/configgen/generators/external/externalGenerator.py
@@ -13,45 +13,49 @@ from utils.logger import eslog
 
 def importExternalGenerator(package, emulator):
     externalGeneratorFilePath = batoceraFiles.retrolxPackages + '/' + package + '/' + emulator + '.py'
-    eslog.log(externalGeneratorFilePath)
     externalGeneratorModuleName = emulator + 'Generator'
-    eslog.log(externalGeneratorModuleName)
     if not os.path.exists(externalGeneratorFilePath):
         eslog.log('Cannot find external generator for package ' + package + ' emulator ' + emulator)
         return None
 
-    eslog.log('Loading ' + externalGeneratorFilePath + ' as module ' + externalGeneratorModuleName)
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    eslog.log('Importing ' + externalGeneratorFilePath + ' as module ' + externalGeneratorModuleName)
+    spec = importlib.util.spec_from_file_location(externalGeneratorModuleName, externalGeneratorFilePath)
     module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
+    sys.modules[externalGeneratorModuleName] = module
     spec.loader.exec_module(module)
     return module
 
 
 class ExternalGenerator(Generator):
 
-    # Main entry of the module
     def generate(self, system, rom, playersControllers, gameResolution):
         # Try to load external generator module
-        module = importExternalGenerator(system.config['emulator'], system.config['emulator'])
+        module = importExternalGenerator(config['emulator'], config['emulator'])
+        generatorName = getattr(module,'getGeneratorName')()
+        generator = getattr(module,generatorName)();
         # Call it as a regular configgen module
-        return module.generate(self, system, rom, playersControllers, gameResolution)
-
+       return generator.generate(system, rom, playersControllers, gameResolution)
 
     def getResolutionMode(self, config):
         # Try to load external generator module
         module = importExternalGenerator(config['emulator'], config['emulator'])
+        generatorName = getattr(module,'getGeneratorName')()
+        generator = getattr(module,generatorName)();
         # Call it as a regular configgen module
-        return module.getResolutionMode(self, config)
+        return generator.getResolutionMode(config)
 
     def getMouseMode(self, config):
         # Try to load external generator module
         module = importExternalGenerator(config['emulator'], config['emulator'])
+        generatorName = getattr(module,'getGeneratorName')()
+        generator = getattr(module,generatorName)();
         # Call it as a regular configgen module
-        return module.getMouseMose(self, config)
+        return generator.getMouseMose(config)
 
     def executionDirectory(self, config, rom):
         # Try to load external generator module
         module = importExternalGenerator(config['emulator'], config['emulator'])
+        generatorName = getattr(module,'getGeneratorName')()
+        generator = getattr(module,generatorName)();
         # Call it as a regular configgen module
-        return module.executionDirectory(module)
+        return generator.executionDirectory(config, rom)
