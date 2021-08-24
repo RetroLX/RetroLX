1facd8f71e946ab1951fdf892ea2e9e2802de5c6
Romain TISSERAND <romain.tisserand@gmail.com>
2021-08-24 14:43:52 +0200

    Add support for Play! (standalone on Xorg, libretro for GLES 3.0+ platforms)

---
+++ package/retrolx/emulators/libretro/libretro-play/libretro-play.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# PLAY
+#
+################################################################################
+
+LIBRETRO_PLAY_VERSION = 3555a1385f754222e34d4f661ebd47347e6f357d
+LIBRETRO_PLAY_SITE = https://github.com/jpd002/Play-.git
+LIBRETRO_PLAY_LICENSE = BSD
+LIBRETRO_PLAY_DEPENDENCIES = 
+
+LIBRETRO_PLAY_SITE_METHOD = git
+LIBRETRO_PLAY_GIT_SUBMODULES = YES
+
+LIBRETRO_PLAY_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+LIBRETRO_PLAY_CONF_OPTS += -DBUILD_SHARED_LIBS=ON
+LIBRETRO_PLAY_CONF_OPTS += -DBUILD_TESTS=OFF
+LIBRETRO_PLAY_CONF_OPTS += -DBUILD_LIBRETRO_CORE=ON
+LIBRETRO_PLAY_CONF_OPTS += -DBUILD_PLAY=OFF
+
+$(eval $(cmake-package))
---
+++ package/retrolx/emulators/play/play.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# PLAY
+#
+################################################################################
+
+PLAY_VERSION = 3555a1385f754222e34d4f661ebd47347e6f357d
+PLAY_SITE = https://github.com/jpd002/Play-.git
+PLAY_LICENSE = BSD
+PLAY_DEPENDENCIES = qt5base qt5x11extras xserver_xorg-server libglew
+
+PLAY_SITE_METHOD = git
+PLAY_GIT_SUBMODULES = YES
+
+PLAY_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+PLAY_CONF_OPTS += -DBUILD_SHARED_LIBS=ON
+PLAY_CONF_OPTS += -DBUILD_TESTS=OFF
+
+$(eval $(cmake-package))
