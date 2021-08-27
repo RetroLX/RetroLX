0762ed71f69dce49d69f201687dc722b62364639
Romain Tisserand <romain.tisserand@gmail.com>
2021-08-27 22:14:16 +0200

    Fix RPCS3 0.0.17 (builds on native only with GLIBC 2.33)

--- package/retrolx/emulators/rpcs3/rpcs3.mk
+++ package/retrolx/emulators/rpcs3/rpcs3.mk
@@ -23,18 +23,19 @@ RPCS3_CONF_OPTS += -DWITH_LLVM=ON
 RPCS3_CONF_OPTS += -DBUILD_LLVM_SUBMODULE=ON
 RPCS3_CONF_OPTS += -DUSE_NATIVE_INSTRUCTIONS=OFF
 RPCS3_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+RPCS3_CONF_OPTS += -DUSE_SYSTEM_FAUDIO=ON
 RPCS3_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
-RPCS3_CONF_ENV += PATH="/x86_64/host/x86_64-buildroot-linux-gnu/sysroot/usr/bin:$$PATH"
-RPCS3_CONF_ENV += LD_LIBRARY_PATH="/x86_64/host/lib:/x86_64/host/usr/lib:$$LD_LIBRARY_PATH"
+RPCS3_CONF_ENV += PATH="$(STAGING_DIR)/usr/bin:$$PATH"
+RPCS3_CONF_ENV += LD_LIBRARY_PATH="$(HOST_DIR)/lib:$(HOST_DIR)/usr/lib:$$LD_LIBRARY_PATH"
 
 define RPCS3_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
 		CC_FOR_BUILD="$(TARGET_CC)" GCC_FOR_BUILD="$(TARGET_CC)" \
 		CXX_FOR_BUILD="$(TARGET_CXX)" LD_FOR_BUILD="$(TARGET_LD)" \
                 CROSS_COMPILE="$(STAGING_DIR)/usr/bin/" LD_LIBRARY_PATH="$(HOST_DIR)/usr/lib:$(HOST_DIR)/lib:$(LD_LIBRARY_PATH)" \
-                PREFIX="/x86_64/host/x86_64-buildroot-linux-gnu/sysroot/" \
-                PKG_CONFIG="/x86_64/host/x86_64-buildroot-linux-gnu/sysroot/usr/bin/pkg-config" \
+                PREFIX="$(STAGING_DIR)" \
+                PKG_CONFIG="$(STAGING_DIR)/usr/bin/pkg-config" \
 		$(MAKE) -C $(@D)
 endef
 
