63bdf2e9da8556dac26af9b45f444f1ff5b977be
Romain Tisserand <romain.tisserand@gmail.com>
2021-08-28 13:29:00 +0200

    Temptative uboot (post-image) Allwinner H3 fix

--- board/batocera/allwinner/h3/cha/build-uboot.sh
+++ board/batocera/allwinner/h3/cha/build-uboot.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+HOST_DIR=$1
+IMAGES_DIR=$2
+
+# Clone U-Boot mainline
 git clone --depth 1 https://source.denx.de/u-boot/u-boot.git -b v2021.07
 cd u-boot
+
+# Make config
 make libretech_all_h3_cc_h3_defconfig
-ARCH=arm CROSS_COMPILE=/h3/host/bin/arm-buildroot-linux-gnueabihf- make -j$(nproc)
+
+# Build it
+ARCH=arm CROSS_COMPILE="${HOST_DIR}/bin/arm-buildroot-linux-gnueabihf-" make -j$(nproc)
 mkdir -p ../../uboot-cha
+
+# Copy to appropriate place
 cp u-boot-sunxi-with-spl.bin ../../uboot-cha/
--- board/batocera/allwinner/h3/orangepi-pc-plus/build-uboot.sh
+++ board/batocera/allwinner/h3/orangepi-pc-plus/build-uboot.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+HOST_DIR=$1
+IMAGES_DIR=$2
+
+# Clone U-Boot mainline
 git clone --depth 1 https://source.denx.de/u-boot/u-boot.git -b v2021.07
 cd u-boot
+
+# Make config
 make orangepi_pc_plus_defconfig
-ARCH=arm CROSS_COMPILE=/h3/host/bin/arm-buildroot-linux-gnueabihf- make -j$(nproc)
+
+# Build it
+ARCH=arm CROSS_COMPILE="${HOST_DIR}/bin/arm-buildroot-linux-gnueabihf-" make -j$(nproc)
 mkdir -p ../../uboot-orangepi-pc-plus
+
+# Copy to appropriate place
 cp u-boot-sunxi-with-spl.bin ../../uboot-orangepi-pc-plus/
