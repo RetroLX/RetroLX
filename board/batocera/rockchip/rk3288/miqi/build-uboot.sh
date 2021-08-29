b059e57c2a402236eaed63a96d65fe2a33f0fdc5
Romain Tisserand <romain.tisserand@gmail.com>
2021-08-29 22:04:26 +0200

    Fixup uboot build scripts for RK3288

--- board/batocera/rockchip/rk3288/miqi/build-uboot.sh
+++ board/batocera/rockchip/rk3288/miqi/build-uboot.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+HOST_DIR=$1
+IMAGES_DIR=$2
+
+# Clone U-Boot mainline
 git clone --depth 1 https://source.denx.de/u-boot/u-boot.git -b v2021.07
 cd u-boot
-ARCH=arm CROSS_COMPILE=/rk3288/host/bin/arm-buildroot-linux-gnueabihf- make -j$(nproc) miqi-rk3288_defconfig all
+
+# Make config
+make miqi-rk3288_defconfig
+
+# Build it
+ARCH=arm CROSS_COMPILE="${HOST_DIR}/bin/arm-buildroot-linux-gnueabihf-" make -j$(nproc) all
 mkdir -p ../../uboot-miqi
+
+# Copy to appropriate place
 cp u-boot-dtb.img ../../uboot-miqi/
-"/rk3288/host/bin/mkimage" -n rk3288 -T rksd -d "spl/u-boot-spl-dtb.bin" "../../uboot-miqi/u-boot-tpl.img" || exit 1
+
+# Generate Rockchip SPL image
+"${HOST_DIR}/bin/mkimage" -n rk3288 -T rksd -d "spl/u-boot-spl-dtb.bin" "../../uboot-miqi/u-boot-tpl.img" || exit 1
 cat "u-boot-dtb.bin" >> "../../uboot-miqi/u-boot-tpl.img" || exit 1
--- board/batocera/rockchip/rk3288/tinkerboard/build-uboot.sh
+++ board/batocera/rockchip/rk3288/tinkerboard/build-uboot.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+HOST_DIR=$1
+IMAGES_DIR=$2
+
+# Clone U-Boot mainline
 git clone --depth 1 https://source.denx.de/u-boot/u-boot.git -b v2021.07
 cd u-boot
-ARCH=arm CROSS_COMPILE=/rk3288/host/bin/arm-buildroot-linux-gnueabihf- make -j$(nproc) tinker-rk3288_defconfig all
-mkdir -p ../../uboot-tinkerboard
-cp u-boot-dtb.img ../../uboot-tinkerboard/
-"/rk3288/host/bin/mkimage" -n rk3288 -T rksd -d "tpl/u-boot-tpl.bin" "../../uboot-tinkerboard/u-boot-tpl.img" || exit 1
-cat "spl/u-boot-spl-dtb.bin" >> "../../uboot-tinkerboard/u-boot-tpl.img" || exit 1
 
+# Make config
+make tinker-rk3288_defconfig
+
+# Build it
+ARCH=arm CROSS_COMPILE="${HOST_DIR}/bin/arm-buildroot-linux-gnueabihf-" make -j$(nproc) all
+mkdir -p ../../uboot-tinkerboard
+
+# Copy to appropriate place
+cp u-boot-dtb.img ../../uboot-tinkerboard/
+
+# Generate Rockchip SPL image
+"${HOST_DIR}/bin/mkimage" -n rk3288 -T rksd -d "tpl/u-boot-tpl.bin" "../../uboot-tinkerboard/u-boot-tpl.img" || exit 1
+cat "spl/u-boot-spl-dtb.bin" >> "../../uboot-tinkerboard/u-boot-tpl.img" || exit 1
