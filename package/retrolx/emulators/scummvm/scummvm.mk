d92fa3d6879491f7efc83efacf6a4f9ccf1acb3f
Romain Tisserand <romain.tisserand@gmail.com>
2021-08-29 22:20:38 +0200

    Move ScummVM to 2.3.0pre (branch-2-3)

--- package/retrolx/emulators/scummvm/scummvm.mk
+++ package/retrolx/emulators/scummvm/scummvm.mk
@@ -3,8 +3,8 @@
 # Scummvm
 #
 ################################################################################
-# Version.: Commits on Sep 27, 2020
-SCUMMVM_VERSION = v2.2.0
+# Version.: Prelease on Aug, 29 2021
+SCUMMVM_VERSION = branch-2-3
 SCUMMVM_SITE = $(call github,scummvm,scummvm,$(SCUMMVM_VERSION))
 SCUMMVM_LICENSE = GPLv2
 SCUMMVM_DEPENDENCIES = sdl2 zlib jpeg libmpeg2 libogg libvorbis flac libmad libpng libtheora faad2 freetype
