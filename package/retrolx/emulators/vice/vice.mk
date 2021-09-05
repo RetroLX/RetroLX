5b8c4e22ba29c83c03d8a82f390b84e4a6ab68db
Romain Tisserand <romain.tisserand@gmail.com>
2021-09-05 16:40:24 +0200

    Move VICE to package system

--- package/retrolx/emulators/vice/vice.mk
+++ package/retrolx/emulators/vice/vice.mk
@@ -10,7 +10,11 @@ VICE_SITE = https://sourceforge.net/projects/vice-emu/files/releases
 VICE_LICENSE = GPLv2
 VICE_DEPENDENCIES = ffmpeg sdl2 libpng giflib zlib lame alsa-lib jpeg sdl2_image
 
-VICE_CONF_OPTS += --disable-option-checking --disable-pdf-docs
+VICE_PKG_DIR = $(TARGET_DIR)/opt/retrolx/vice
+VICE_PKG_INSTALL_DIR = /userdata/packages/$(RETROLX_SYSTEM_ARCH)/vice
+VICE_PREFIX_DIR = /opt/retrolx/vice$(VICE_PKG_INSTALL_DIR)
+
+VICE_CONF_OPTS += --disable-option-checking --disable-pdf-docs --prefix=$(VICE_PREFIX_DIR) --exec-prefix=$(VICE_PREFIX_DIR)
 
 # FFMPEG
 VICE_DEPENDENCIES += ffmpeg
@@ -31,5 +35,18 @@ VICE_CONF_OPTS += --disable-debug-gtk3ui
 VICE_CONF_OPTS += --disable-native-gtk3ui
 
 VICE_CONF_ENV += LDFLAGS=-lSDL2
+
+define VICE_MAKEPKG
+	# Build Pacman package
+	cd $(VICE_PKG_DIR) && $(BR2_EXTERNAL_RETROLX_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/emulators/vice/PKGINFO \
+	$(RETROLX_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_RETROLX_PATH)/repo/$(RETROLX_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
+endef
+
+VICE_POST_INSTALL_TARGET_HOOKS = VICE_MAKEPKG
 
 $(eval $(autotools-package))
