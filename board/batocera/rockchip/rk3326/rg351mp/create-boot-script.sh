6f23e39421f0b821e1094a81b58696e96996768e
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-02 13:28:21 +0200

    WIP RK3326 mainline uboot

--- board/batocera/rockchip/rk3326/rg351mp/create-boot-script.sh
+++ board/batocera/rockchip/rk3326/rg351mp/create-boot-script.sh
@@ -16,16 +16,22 @@ TARGET_DIR=$5
 BATOCERA_BINARIES_DIR=$6
 
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot" || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/packages" || exit 1
 cp -r "${BUILD_DIR}"/repo/* "${BATOCERA_BINARIES_DIR}/boot/packages/" || exit 1
 
-"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${BATOCERA_BINARIES_DIR}/boot/boot/linux" || exit 1
-cp "${BINARIES_DIR}/uInitrd"         "${BATOCERA_BINARIES_DIR}/boot/boot/uInitrd"         || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/uboot" || exit 1
+cp "${BOARD_DIR}/build-uboot.sh"          "${BATOCERA_BINARIES_DIR}/uboot/" || exit 1
+cd "${BATOCERA_BINARIES_DIR}/uboot/" && ./build-uboot.sh "${HOST_DIR}" "${BINARIES_DIR}" || exit 1
+
+#"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${BATOCERA_BINARIES_DIR}/boot/boot/linux" || exit 1
+cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"       "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.gz"       || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
 cp "${BINARIES_DIR}/modules"         "${BATOCERA_BINARIES_DIR}/boot/boot/modules"         || exit 1
 
-cp "${BINARIES_DIR}/rk3326-rg351p.dtb"         "${BATOCERA_BINARIES_DIR}/boot/boot/rk3326-rg351p.dtb"         || exit 1
+cp "${BINARIES_DIR}/rk3326-rg351mp.dtb"         "${BATOCERA_BINARIES_DIR}/boot/boot/rk3326-rg351mp.dtb"         || exit 1
 
-cp "${BOARD_DIR}/boot/boot.ini"                     "${BATOCERA_BINARIES_DIR}/boot/"                          || exit 1
+cp "${BOARD_DIR}/boot/boot.ini"                "${BATOCERA_BINARIES_DIR}/boot/"                       || exit 1
 
 exit 0
