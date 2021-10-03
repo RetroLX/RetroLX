6dd77bf61023fc5df5f1d5d38b10debcec270541
Romain Tisserand <romain.tisserand@gmail.com>
2021-10-04 00:10:51 +0200

    Rock64 uboot with ATF 2.5

---
+++ board/batocera/rockchip/rk3328/rock64/build-uboot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+HOST_DIR=$1
+BOARD_DIR=$2
+IMAGES_DIR=$3
+
+# ARM Trusted Firmware BL31
+export BL31="${IMAGES_DIR}/bl31.elf"
+
+# Clone U-Boot mainline
+wget "https://ftp.denx.de/pub/u-boot/u-boot-2021.07.tar.bz2"
+tar xf u-boot-2021.07.tar.bz2
+cd u-boot-2021.07
+
+# Apply patches
+PATCHES="${BOARD_DIR}/patches/uboot/*.patch"
+for patch in $PATCHES
+do
+  echo "Applying patch: $patch"
+  patch -p1 < $patch
+done
+
+# Make config
+make rock64-rk3328_defconfig
+
+# Build it
+ARCH=aarch64 CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-" make -j$(nproc) all
+mkdir -p ../../uboot-rock64
+
+# Copy to appropriate place
+cp u-boot-rockchip.bin ../../uboot-rock64/
+#cp u-boot-dtb.img ../../uboot-rock64/
+
+# Generate Rockchip SPL image
+#"${HOST_DIR}/bin/mkimage" -n px30 -T rksd -d "tpl/u-boot-tpl.bin" "../../uboot-rock64/u-boot-tpl.img" || exit 1
+#cat "spl/u-boot-spl-dtb.bin" >> "../../uboot-rock64/u-boot-tpl.img" || exit 1
