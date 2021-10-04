8cfcdb95f5ecf2a9bbdfa2c71315494e11d21d48
Romain Tisserand <romain.tisserand@gmail.com>
2021-10-04 21:14:02 +0200

    Revert "WIP RK3326 mainline uboot"
    This reverts commit 6f23e39421f0b821e1094a81b58696e96996768e.

---
+++ package/batocera/boot/uboot-odroid-goa/uboot-odroid-goa.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# uboot files for odroid go advance
+#
+################################################################################
+
+UBOOT_ODROID_GOA_VERSION = 0e26e35cb18a80005b7de45c95858c86a2f7f41e
+UBOOT_ODROID_GOA_SITE = https://github.com/hardkernel/u-boot.git
+UBOOT_ODROID_GOA_SITE_METHOD=git
+
+UBOOT_ODROID_GOA_DEPENDENCIES = host-toolchain-optional-linaro-aarch64
+
+define UBOOT_ODROID_GOA_BUILD_CMDS
+        cd $(@D) && $(@D)/make.sh odroidgoa
+endef
+
+define UBOOT_ODROID_GOA_INSTALL_TARGET_CMDS
+	mkdir -p $(BINARIES_DIR)/odroidgoa
+	cp $(@D)/sd_fuse/idbloader.img $(BINARIES_DIR)/odroidgoa/idbloader.img
+	cp $(@D)/sd_fuse/uboot.img     $(BINARIES_DIR)/odroidgoa/uboot.img
+	cp $(@D)/sd_fuse/trust.img     $(BINARIES_DIR)/odroidgoa/trust.img
+endef
+
+$(eval $(generic-package))
