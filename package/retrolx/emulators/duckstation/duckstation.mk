3a34c0477ca9ff3aa587882732328f607b7bd0b6
Romain Tisserand <romain.tisserand@gmail.com>
2021-10-05 13:30:49 +0200

    Bump DuckStation

--- package/retrolx/emulators/duckstation/duckstation.mk
+++ package/retrolx/emulators/duckstation/duckstation.mk
@@ -3,8 +3,8 @@
 # DUCKSTATION
 #
 ################################################################################
-# Version.: Commits on Sep 25, 2021
-DUCKSTATION_VERSION = 8864b48c02c5d186a4e271cd7e94f7fec870fe33
+# Version.: Commits on Oct 1, 2021
+DUCKSTATION_VERSION = a7096f033ecca48827fa55825fc0d0221265f1c2
 DUCKSTATION_SITE = https://github.com/stenzek/duckstation.git
 
 DUCKSTATION_DEPENDENCIES = fmt boost ffmpeg ecm
