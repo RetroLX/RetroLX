c88faf284b1d79022cf5621fe5410a0ae6d6de82
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-10-07 03:41:14 +0300

    Bump Dolphin to the latest commit

--- package/retrolx/emulators/dolphin-emu/dolphin-emu.mk
+++ package/retrolx/emulators/dolphin-emu/dolphin-emu.mk
@@ -3,8 +3,8 @@
 # DOLPHIN EMU
 #
 ################################################################################
-# Version: Commits on Sep 03, 2021 - 5.0-15015
-DOLPHIN_EMU_VERSION = 4b8b53ac732645f6c0fd3f4b11733cb3d86c9c41
+# Version: Commits on Oct 07, 2021 - 5.0-15015
+DOLPHIN_EMU_VERSION = bf6a278c040ce6dfb8269c6c225ec44178b1800a
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
 DOLPHIN_EMU_LICENSE = GPLv2+
 DOLPHIN_EMU_DEPENDENCIES = libevdev ffmpeg zlib libpng lzo libusb libcurl bluez5_utils hidapi xz host-xz
