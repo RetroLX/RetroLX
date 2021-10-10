5f850e23dfa2b8bed12c21b066239b1a0f5e1749
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-10 22:48:39 +0200

    Enable pulse in RPCS3, move to package system

--- package/retrolx/emulators/rpcs3/rpcs3.mk
+++ package/retrolx/emulators/rpcs3/rpcs3.mk
@@ -13,7 +13,11 @@ RPCS3_GIT_SUBMODULES=YES
 RPCS3_LICENSE = GPLv2
 RPCS3_DEPENDENCIES = qt5declarative libxml2 mesa3d libglu openal alsa-lib libevdev libglew libusb ffmpeg faudio wolfssl
 
-RPCS3_CONF_OPTS += -DUSE_PULSE=OFF
+RPCS3_PKG_DIR = $(TARGET_DIR)/opt/retrolx/rpcs3
+RPCS3_PKG_INSTALL_DIR = /userdata/packages/$(RETROLX_SYSTEM_ARCH)/rpcs3
+RPCS3_PREFIX_DIR = /opt/retrolx/rpcs3$(RPCS3_PKG_INSTALL_DIR)
+
+RPCS3_CONF_OPTS += -DUSE_PULSE=ON
 RPCS3_CONF_OPTS += -DUSE_SYSTEM_FFMPEG=ON
 RPCS3_CONF_OPTS += -DUSE_SYSTEM_LIBPNG=ON
 RPCS3_CONF_OPTS += -DUSE_DISCORD_RPC=OFF
@@ -30,6 +34,9 @@ RPCS3_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 RPCS3_CONF_ENV += PATH="$(STAGING_DIR)/usr/bin:$$PATH"
 RPCS3_CONF_ENV += LD_LIBRARY_PATH="$(HOST_DIR)/lib:$(HOST_DIR)/usr/lib:$$LD_LIBRARY_PATH"
 
+# Install into package prefix
+RPCS3_INSTALL_TARGET_OPTS = DESTDIR="$(RPCS3_PKG_DIR)$(RPCS3_PKG_INSTALL_DIR)" install
+
 define RPCS3_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
 		CC_FOR_BUILD="$(TARGET_CC)" GCC_FOR_BUILD="$(TARGET_CC)" \
@@ -40,11 +47,20 @@ define RPCS3_BUILD_CMDS
 		$(MAKE) -C $(@D)
 endef
 
-define RPCS3_INSTALL_EVMAPY
+define RPCS3_MAKEPKG
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/emulators/rpcs3/evmapy.keys $(TARGET_DIR)/usr/share/evmapy/ps3.keys
+	
+	# Build Pacman package
+	cd $(SOLARUS_ENGINE_PKG_DIR) && $(BR2_EXTERNAL_RETROLX_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/emulators/rpcs3/PKGINFO \
+	$(RETROLX_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_RETROLX_PATH)/repo/$(RETROLX_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
 
-RPCS3_POST_INSTALL_TARGET_HOOKS = RPCS3_INSTALL_EVMAPY
+RPCS3_POST_INSTALL_TARGET_HOOKS = RPCS3_MAKEPKG
 
 $(eval $(cmake-package))
