2c7bd02227291e6c94dda71ead3540cb047e2d4b
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-21 23:44:44 +0200

    Switch FSUAE to stable 3.1.35 version

--- package/retrolx/emulators/fsuae/fsuae.mk
+++ package/retrolx/emulators/fsuae/fsuae.mk
@@ -3,8 +3,8 @@
 # FSUAE
 #
 ################################################################################
-# Version.: Commits on Jun 11, 2021 (4.0.x)
-FSUAE_VERSION = 80f746286d9a9347ab8252a7cb7c45df3e887fd2
+# Version.: Release on Oct 11, 2021
+FSUAE_VERSION = v3.1.35
 FSUAE_SITE = $(call github,FrodeSolheim,fs-uae,$(FSUAE_VERSION))
 FSUAE_LICENSE = GPLv2
 FSUAE_DEPENDENCIES = xserver_xorg-server openal libpng sdl2 zlib libmpeg2 libglib2 libcapsimage
