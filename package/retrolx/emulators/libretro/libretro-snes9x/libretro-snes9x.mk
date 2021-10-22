10abc9bb784956da27fbfb094042763edaaf3543
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-22 22:58:02 +0200

    Bump lr-snes9x

--- package/retrolx/emulators/libretro/libretro-snes9x/libretro-snes9x.mk
+++ package/retrolx/emulators/libretro/libretro-snes9x/libretro-snes9x.mk
@@ -3,8 +3,8 @@
 # SNES9X
 #
 ################################################################################
-# Version.: Commits on Oct 10, 2021
-LIBRETRO_SNES9X_VERSION = 2e3876cd0fe472cbbc967de103895e70d1ca98c4
+# Version.: Commits on Oct 19, 2021
+LIBRETRO_SNES9X_VERSION = 7ad2605f19864ed179282a1ab466cc7d4b404994
 LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
 LIBRETRO_SNES9X_LICENSE = Non-commercial
 
