056910d732e6e0eed0aeeec843d1bada9f2482a0
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-24 23:16:25 +0200

    Bump lr-blastem + enable (NEW_CORE) on any architecture

--- package/retrolx/emulators/libretro/libretro-blastem/libretro-blastem.mk
+++ package/retrolx/emulators/libretro/libretro-blastem/libretro-blastem.mk
@@ -3,8 +3,8 @@
 # BLASTEM
 #
 ################################################################################
-# Version.: Commits on Mar 09, 2021
-LIBRETRO_BLASTEM_VERSION = a61b47d5489e
+# Version.: Commits on Oct 21, 2021
+LIBRETRO_BLASTEM_VERSION = c6ffbe21a169
 LIBRETRO_BLASTEM_SOURCE = $(LIBRETRO_BLASTEM_VERSION).tar.gz
 LIBRETRO_BLASTEM_SITE = https://www.retrodev.com/repos/blastem/archive
 LIBRETRO_BLASTEM_LICENSE = Non-commercial
@@ -12,17 +12,25 @@ LIBRETRO_BLASTEM_LICENSE = Non-commercial
 LIBRETRO_BLASTEM_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
 LIBRETRO_BLASTEM_PKG_INSTALL_DIR = /userdata/packages/$(RETROLX_SYSTEM_ARCH)/lr-blastem
 
-LIBRETRO_BLASTEM_EXTRAOPTS=""
+LIBRETRO_BLASTEM_EXTRAOPTS=
 
 ifeq ($(BR2_x86_64),y)
 LIBRETRO_BLASTEM_EXTRAOPTS=CPU=x86_64
 else  ifeq ($(BR2_x86_i686),y)
 LIBRETRO_BLASTEM_EXTRAOPTS=CPU=i686
+else ifeq ($(BR2_arm),y)
+LIBRETRO_BLASTEM_EXTRAOPTS=CPU=arm
+else ifeq ($(BR2_aarch64),y)
+LIBRETRO_BLASTEM_EXTRAOPTS=CPU=aarch64
+else
+LIBRETRO_BLASTEM_EXTRAOPTS=CPU=unknown
 endif
 
 define LIBRETRO_BLASTEM_BUILD_CMDS
-    $(SED) "s+CPU:=i686+CPU?=i686+g" $(@D)/Makefile
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) libblastem.so $(LIBRETRO_BLASTEM_EXTRAOPTS)
+	$(SED) "s+CPU:=i686+CPU?=i686+g" $(@D)/Makefile
+	cd $(@D) && ./cpu_dsl.py z80.cpu > z80.c
+	cd $(@D) && ./cpu_dsl.py m68k.cpu > m68k.c
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CFLAGS="-DHAS_PROC -DHAVE_UNISTD_H -fPIC -DNEW_CORE -DIS_LIB -std=gnu99" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) $(LIBRETRO_BLASTEM_EXTRAOPTS) libblastem.so
 endef
 
 define LIBRETRO_BLASTEM_MAKEPKG
