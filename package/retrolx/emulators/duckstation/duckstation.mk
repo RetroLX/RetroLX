0428c159ef36fe61b11a70449eeaf766332d1a14
Romain TISSERAND <romain.tisserand@gmail.com>
2021-10-25 13:53:39 +0200

    Bump duckstation standalone, try disabled horrible rk3326 rotation patch (should be handled by wayland)

--- package/retrolx/emulators/duckstation/duckstation.mk
+++ package/retrolx/emulators/duckstation/duckstation.mk
@@ -3,8 +3,8 @@
 # DUCKSTATION
 #
 ################################################################################
-# Version.: Commits on Oct 1, 2021
-DUCKSTATION_VERSION = a7096f033ecca48827fa55825fc0d0221265f1c2
+# Version.: Commits on Oct 25, 2021
+DUCKSTATION_VERSION = 48ddebd82e1029876661010bd6c81f49c45f4182
 DUCKSTATION_SITE = https://github.com/stenzek/duckstation.git
 
 DUCKSTATION_DEPENDENCIES = fmt boost ffmpeg ecm
