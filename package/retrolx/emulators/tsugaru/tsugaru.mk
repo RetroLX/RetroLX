f0f3e6fddd3f1366eebbc2da7d45a908ec97d221
Romain Tisserand <romain.tisserand@gmail.com>
2021-10-27 21:53:14 +0200

    Bump tsugaru

--- package/retrolx/emulators/tsugaru/tsugaru.mk
+++ package/retrolx/emulators/tsugaru/tsugaru.mk
@@ -3,8 +3,8 @@
 # TSUGARU
 #
 ################################################################################
-# Version.: Commits on Sep 29, 2021
-TSUGARU_VERSION = v20210929
+# Version.: Commits on Oct 26, 2021
+TSUGARU_VERSION = v20211026
 TSUGARU_SITE = $(call github,captainys,TOWNSEMU,$(TSUGARU_VERSION))
 TSUGARU_DEPENDENCIES = libglu
 TSUGARU_LICENSE = GPLv2
