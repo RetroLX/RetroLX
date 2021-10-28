387edf147523c79f1202615512cd0c8c8a51421b
Romain Tisserand <romain.tisserand@gmail.com>
2021-10-28 10:44:47 +0200

    Big GPU rework (NVIDIA GBM driver, enable roughly Vulkan on Panfrost)

--- package/retrolx/gpu/vulkan/vulkan-samples/vulkan-samples.mk
+++ package/retrolx/gpu/vulkan/vulkan-samples/vulkan-samples.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VULKAN_SAMPLES_VERSION = 47c5237be21209afc945e43d03ae409fa80a0155
+VULKAN_SAMPLES_VERSION = fa283171bd5887613dac10c4b3d164fba4b14887
 VULKAN_SAMPLES_SITE =  https://github.com/KhronosGroup/Vulkan-Samples
 VULKAN_SAMPLES_GIT_SUBMODULES=YES
 VULKAN_SAMPLES_SITE_METHOD=git
