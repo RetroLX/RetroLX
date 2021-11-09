81fa62d9c2400314a1f8b0f3bea62333d4528aa1
Romain TISSERAND <romain.tisserand@gmail.com>
2021-11-09 22:56:15 +0100

    Move cdogs-sdl to package system

--- package/retrolx/ports/cdogs/cdogs.mk
+++ package/retrolx/ports/cdogs/cdogs.mk
@@ -10,27 +10,34 @@ CDOGS_SITE = $(call github,cxong,cdogs-sdl,$(CDOGS_VERSION))
 CDOGS_DEPENDENCIES = sdl2 host-python-protobuf
 CDOGS_LICENSE = GPL-2.0
 
+CDOGS_PKG_DIR = $(TARGET_DIR)/opt/retrolx/cdogs
+CDOGS_PKG_INSTALL_DIR = /userdata/packages/$(RETROLX_SYSTEM_ARCH)/cdogs
+
+# Install into package prefix
+CDOGS_INSTALL_TARGET_OPTS = DESTDIR="$(CDOGS_PKG_DIR)$(CDOGS_PKG_INSTALL_DIR)" install
+
 CDOGS_SUPPORTS_IN_SOURCE_BUILD = NO
 
 CDOGS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-CDOGS_CONF_OPTS += -DCDOGS_DATA_DIR=/userdata/roms/cdogs/
+CDOGS_CONF_OPTS += -DCDOGS_DATA_DIR=$(CDOGS_PKG_DIR)$(CDOGS_PKG_INSTALL_DIR)
 CDOGS_CONF_OPTS += -DBUILD_EDITOR=OFF
 
-define CDOGS_INSTALL_TARGET_CMDS
-    $(INSTALL) -D $(@D)/buildroot-build/src/cdogs-sdl $(TARGET_DIR)/usr/bin/cdogs
-    mkdir -p $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -pav $(@D)/data $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -pav $(@D)/doc $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -pav $(@D)/dogfights $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -pav $(@D)/graphics $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -pav $(@D)/missions $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -pav $(@D)/music $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -pav $(@D)/sounds $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs
-    cp -f $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/cdogs/gamecontrollerdb.txt $(TARGET_DIR)/usr/share/retrolx/datainit/roms/cdogs/data
+define CDOGS_MAKEPKG
+	# Tidy up package
+	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/cdogs/*.py $(CDOGS_PKG_DIR)$(CDOGS_PKG_INSTALL_DIR)
+	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/cdogs/cdogs.keys $(CDOGS_PKG_DIR)$(CDOGS_PKG_INSTALL_DIR)
+	cp -f $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/cdogs/gamecontrollerdb.txt $(CDOGS_PKG_DIR)$(CDOGS_PKG_INSTALL_DIR)
 
-    # evmap config
-	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/cdogs/cdogs.keys $(TARGET_DIR)/usr/share/evmapy
+	# Build Pacman package
+	cd $(CDOGS_PKG_DIR) && $(BR2_EXTERNAL_RETROLX_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/cdogs/PKGINFO \
+	$(RETROLX_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_RETROLX_PATH)/repo/$(RETROLX_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
+
+CDOGS_POST_INSTALL_TARGET_HOOKS = CDOGS_MAKEPKG
 
 $(eval $(cmake-package))
