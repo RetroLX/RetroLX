5df3c3d64883096b6f98b3a8ed5d6cb02586ed3d
Romain TISSERAND <romain.tisserand@gmail.com>
2021-11-09 18:46:19 +0100

    Move Hurrian to package system

--- package/retrolx/ports/hurrican/hurrican.mk
+++ package/retrolx/ports/hurrican/hurrican.mk
@@ -12,21 +12,38 @@ HURRICAN_SITE_METHOD=git
 HURRICAN_GIT_SUBMODULES=YES
 HURRICAN_LICENSE = GPL-2.0
 
+HURRICAN_PKG_DIR = $(TARGET_DIR)/opt/retrolx/hurrican
+HURRICAN_PKG_INSTALL_DIR = /userdata/packages/$(RETROLX_SYSTEM_ARCH)/hurrican
+
+# Install into package prefix
+HURRICAN_INSTALL_TARGET_OPTS = DESTDIR="$(HURRICAN_PKG_DIR)$(HURRICAN_PKG_INSTALL_DIR)" install
+
 HURRICAN_SUPPORTS_IN_SOURCE_BUILD = NO
 
 HURRICAN_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release $(@D)/Hurrican -DRENDERER=GLES2
 
-define HURRICAN_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/hurrican/
-	cp -pvr $(@D)/buildroot-build/hurrican $(TARGET_DIR)/usr/bin
-	chmod 0755 $(TARGET_DIR)/usr/bin/hurrican
-    cp -avr $(@D)/Hurrican/data $(TARGET_DIR)/usr/share/hurrican/
-    cp -avr $(@D)/Hurrican/lang $(TARGET_DIR)/usr/share/hurrican/
+define HURRICAN_MAKEPKG
+	# Copy data
+	#mkdir -p $(TARGET_DIR)/usr/share/hurrican/
+	#cp -pvr $(@D)/buildroot-build/hurrican $(TARGET_DIR)/usr/bin
+	#chmod 0755 $(TARGET_DIR)/usr/bin/hurrican
+	#cp -avr $(@D)/Hurrican/data $(TARGET_DIR)/usr/share/hurrican
+	#cp -avr $(@D)/Hurrican/lang $(TARGET_DIR)/usr/share/hurrican/
 
-	# evmap config
-	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	# Tidy up package
+	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/hurrican/*.py $(HURRICAN_PKG_DIR)$(HURRICAN_PKG_INSTALL_DIR)
 	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/hurrican/hurrican.keys $(TARGET_DIR)/usr/share/evmapy
+
+	# Build Pacman package
+	cd $(HURRICAN_PKG_DIR) && $(BR2_EXTERNAL_RETROLX_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/ports/hurrican/PKGINFO \
+	$(RETROLX_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_RETROLX_PATH)/repo/$(RETROLX_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
 
-$(eval $(cmake-package))
+HURRICAN_POST_INSTALL_TARGET_HOOKS = HURRICAN_MAKEPKG
 
+$(eval $(cmake-package))
