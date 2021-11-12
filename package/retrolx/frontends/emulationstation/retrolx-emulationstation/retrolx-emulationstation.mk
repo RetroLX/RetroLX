ca93a6406ba49f93601ae84218e470711aadd8fa
Romain TISSERAND <romain.tisserand@gmail.com>
2021-11-12 22:05:24 +0100

    Move EmulationStation to package

--- package/retrolx/frontends/emulationstation/retrolx-emulationstation/retrolx-emulationstation.mk
+++ package/retrolx/frontends/emulationstation/retrolx-emulationstation/retrolx-emulationstation.mk
@@ -15,7 +15,10 @@ RETROLX_EMULATIONSTATION_DEPENDENCIES = sdl2 sdl2_mixer freetype alsa-lib libcur
 RETROLX_EMULATIONSTATION_INSTALL_STAGING = YES
 # RETROLX_EMULATIONSTATION_OVERRIDE_SRCDIR = /sources/batocera-emulationstation
 
-RETROLX_EMULATIONSTATION_CONF_OPTS += -DCMAKE_CXX_FLAGS=-D$(call UPPERCASE,$(RETROLX_SYSTEM_ARCH)) -DCMAKE_BUILD_TYPE=Debug
+RETROLX_EMULATIONSTATION_PKG_DIR = $(TARGET_DIR)/opt/retrolx/emulationstation
+RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR = /userdata/packages/$(RETROLX_SYSTEM_ARCH)/emulationstation
+
+RETROLX_EMULATIONSTATION_CONF_OPTS += -DCMAKE_CXX_FLAGS=-D$(call UPPERCASE,$(RETROLX_SYSTEM_ARCH)) -DCMAKE_BUILD_TYPE=Debug #-DCMAKE_BUILD_TYPE=Release
 
 ifeq ($(BR2_PACKAGE_HAS_LIBMALI),y)
 RETROLX_EMULATIONSTATION_CONF_OPTS += -DCMAKE_EXE_LINKER_FLAGS=-lmali -DCMAKE_SHARED_LINKER_FLAGS=-lmali
@@ -65,27 +68,42 @@ define RETROLX_EMULATIONSTATION_RPI_FIXUP
 	$(SED) 's|.{CMAKE_FIND_ROOT_PATH}/usr|$(STAGING_DIR)/usr|g'    $(@D)/CMakeLists.txt
 endef
 
-define RETROLX_EMULATIONSTATION_RESOURCES
-	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/emulationstation/resources/help
-	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/emulationstation/resources/flags
-	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/emulationstation/resources/battery
-	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/emulationstation/resources/services
-	$(INSTALL) -m 0644 -D $(@D)/resources/*.* $(TARGET_DIR)/usr/share/emulationstation/resources
-	$(INSTALL) -m 0644 -D $(@D)/resources/help/*.* $(TARGET_DIR)/usr/share/emulationstation/resources/help
-	$(INSTALL) -m 0644 -D $(@D)/resources/flags/*.* $(TARGET_DIR)/usr/share/emulationstation/resources/flags
-	$(INSTALL) -m 0644 -D $(@D)/resources/battery/*.* $(TARGET_DIR)/usr/share/emulationstation/resources/battery
-	$(INSTALL) -m 0644 -D $(@D)/resources/services/*.* $(TARGET_DIR)/usr/share/emulationstation/resources/services
+define RETROLX_EMULATIONSTATION_INSTALL_TARGET_CMDS
+	echo "EmulationStation built as package, no rootfs install"
+endef
+
+define RETROLX_EMULATIONSTATION_MAKEPKG
+	$(INSTALL) -m 0755 -d $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/help
+	$(INSTALL) -m 0755 -d $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/flags
+	$(INSTALL) -m 0755 -d $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/battery
+	$(INSTALL) -m 0755 -d $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/services
+	$(INSTALL) -m 0755 -D $(@D)/emulationstation $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)
+	$(INSTALL) -m 0755 -D $(@D)/libpugixml* $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)
+	$(INSTALL) -m 0644 -D $(@D)/resources/*.* $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources
+	$(INSTALL) -m 0644 -D $(@D)/resources/help/*.* $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/help
+	$(INSTALL) -m 0644 -D $(@D)/resources/flags/*.* $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/flags
+	$(INSTALL) -m 0644 -D $(@D)/resources/battery/*.* $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/battery
+	$(INSTALL) -m 0644 -D $(@D)/resources/services/*.* $(RETROLX_EMULATIONSTATION_PKG_DIR)$(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/resources/services
 
 	# es_input.cfg
 	mkdir -p $(TARGET_DIR)/usr/share/retrolx/datainit/system/configs/emulationstation
 	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/frontends/emulationstation/retrolx-emulationstation/controllers/es_input.cfg \
 		$(TARGET_DIR)/usr/share/retrolx/datainit/system/configs/emulationstation
+
+	# Build Pacman package
+	cd $(RETROLX_EMULATIONSTATION_PKG_DIR) && $(BR2_EXTERNAL_RETROLX_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/frontends/emulationstation/retrolx-emulationstation/PKGINFO \
+	$(RETROLX_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_RETROLX_PATH)/repo/$(RETROLX_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
 endef
 
 ### S31emulationstation
 # default for most of architectures
 RETROLX_EMULATIONSTATION_PREFIX = SDL_NOMOUSE=1
-RETROLX_EMULATIONSTATION_CMD = /usr/bin/emulationstation
+RETROLX_EMULATIONSTATION_CMD = $(RETROLX_EMULATIONSTATION_PKG_INSTALL_DIR)/emulationstation
 RETROLX_EMULATIONSTATION_ARGS = --no-splash $${EXTRA_OPTS}
 RETROLX_EMULATIONSTATION_POSTFIX = \&
 RETROLX_EMULATIONSTATION_CONF_OPTS += -DCEC=OFF
@@ -127,7 +145,6 @@ define RETROLX_EMULATIONSTATION_BOOT
 endef
 
 RETROLX_EMULATIONSTATION_PRE_CONFIGURE_HOOKS += RETROLX_EMULATIONSTATION_RPI_FIXUP
-RETROLX_EMULATIONSTATION_POST_INSTALL_TARGET_HOOKS += RETROLX_EMULATIONSTATION_RESOURCES
 RETROLX_EMULATIONSTATION_POST_INSTALL_TARGET_HOOKS += RETROLX_EMULATIONSTATION_BOOT
-
+RETROLX_EMULATIONSTATION_POST_INSTALL_TARGET_HOOKS += RETROLX_EMULATIONSTATION_MAKEPKG
 $(eval $(cmake-package))
