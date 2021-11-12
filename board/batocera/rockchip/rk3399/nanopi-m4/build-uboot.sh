30f9804554e4379dab62678cd05583033da5f158
Romain TISSERAND <romain.tisserand@gmail.com>
2021-11-12 20:43:02 +0100

    Add NanoPi M4 target (will be able to test it soon)

---
+++ board/batocera/rockchip/rk3399/nanopi-m4/build-uboot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+HOST_DIR=$1
+BOARD_DIR=$2
+IMAGES_DIR=$3
+
+# ARM Trusted Firmware BL31
+export BL31="${IMAGES_DIR}/atf/rk3399/bl31.elf"
+
+# Clone Rockchip U-Boot
+git clone https://github.com/rockchip-linux/u-boot/ -b next-dev
+cd u-boot
+
+# Clone mrfixit2001 U-Boot Builder
+git clone https://github.com/mrfixit2001/uboot_builder
+
+# Apply patches
+PATCHES="${BOARD_DIR}/patches/uboot/*.patch"
+for patch in $PATCHES
+do
+  echo "Applying patch: $patch"
+  patch -p1 < $patch
+done
+
+# Build U-Boot
+ARCH=aarch64 CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-" make -j$(nproc) mrproper evb-rk3399_defconfig all
+
+# Rockchip process
+cp u-boot-dtb.bin ./uboot_builder/rk3399
+cd uboot_builder/rk3399
+./make-uboot.sh
+
+# Copy to appropriate place
+mkdir -p ../../../../uboot-evb-rk3399
+mv *.img ../../../../uboot-evb-rk3399
