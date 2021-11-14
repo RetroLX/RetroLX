ccdb2c9f36c9bef8418a3fdb79be31f1430bd41f
Romain Tisserand <romain.tisserand@gmail.com>
2021-11-14 16:40:47 +0100

    Fix s905gen2 build (add khadas-vim2 + missing board.png for x96-max)

---
+++ board/batocera/amlogic/s905gen2/khadas-vim2/build-uboot.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+HOST_DIR=$1
+BOARD_DIR=$2
+IMAGES_DIR=$3
+
+# Download U-Boot mainline
+wget "https://ftp.denx.de/pub/u-boot/u-boot-2021.10.tar.bz2"
+tar xf u-boot-2021.10.tar.bz2
+cd u-boot-2021.10
+
+# Apply patches
+PATCHES="${BOARD_DIR}/patches/uboot/*.patch"
+for patch in $PATCHES
+do
+  echo "Applying patch: $patch"
+  patch -p1 < $patch
+done
+
+# Make config
+make khadas-vim2_defconfig
+
+# Build it
+ARCH=aarch64 CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-" make -j$(nproc)
+mkdir -p ../../uboot-khadas-vim2
+
+# Clone LibreElec Amlogic FIP
+git clone --depth 1 https://github.com/LibreELEC/amlogic-boot-fip
+
+# Sign U-Boot build with Amlogic process
+AML_FIP_DIR="amlogic-boot-fip/khadas-vim2"
+AML_ENCRYPT_BIN="aml_encrypt_gxl"
+cp u-boot.bin ${AML_FIP_DIR}/bl33.bin
+${AML_FIP_DIR}/blx_fix.sh ${AML_FIP_DIR}/bl30.bin \
+  ${AML_FIP_DIR}/zero_tmp \
+  ${AML_FIP_DIR}/bl30_zero.bin \
+  ${AML_FIP_DIR}/bl301.bin \
+  ${AML_FIP_DIR}/bl301_zero.bin \
+  ${AML_FIP_DIR}/bl30_new.bin bl30
+${HOST_DIR}/bin/python ${AML_FIP_DIR}/acs_tool.py ${AML_FIP_DIR}/bl2.bin ${AML_FIP_DIR}/bl2_acs.bin ${AML_FIP_DIR}/acs.bin 0
+${AML_FIP_DIR}/blx_fix.sh ${AML_FIP_DIR}/bl2_acs.bin \
+  ${AML_FIP_DIR}/zero_tmp \
+  ${AML_FIP_DIR}/bl2_zero.bin \
+  ${AML_FIP_DIR}/bl21.bin \
+  ${AML_FIP_DIR}/bl21_zero.bin \
+  ${AML_FIP_DIR}/bl2_new.bin bl2
+${AML_FIP_DIR}/${AML_ENCRYPT_BIN} --bl3enc --input ${AML_FIP_DIR}/bl30_new.bin
+${AML_FIP_DIR}/${AML_ENCRYPT_BIN} --bl3enc --input ${AML_FIP_DIR}/bl31.img
+${AML_FIP_DIR}/${AML_ENCRYPT_BIN} --bl3enc --input ${AML_FIP_DIR}/bl33.bin
+${AML_FIP_DIR}/${AML_ENCRYPT_BIN} --bl2sig --input ${AML_FIP_DIR}/bl2_new.bin --output ${AML_FIP_DIR}/bl2.n.bin.sig
+${AML_FIP_DIR}/${AML_ENCRYPT_BIN} --bootmk \
+  --output ${AML_FIP_DIR}/u-boot.bin \
+  --bl2 ${AML_FIP_DIR}/bl2.n.bin.sig \
+  --bl30 ${AML_FIP_DIR}/bl30_new.bin.enc \
+  --bl31 ${AML_FIP_DIR}/bl31.img.enc \
+  --bl33 ${AML_FIP_DIR}/bl33.bin.enc
+
+# Copy to appropriate place
+cp ${AML_FIP_DIR}/u-boot.bin.sd.bin ../../uboot-khadas-vim2/
+
---
+++ board/batocera/amlogic/s905gen2/khadas-vim2/create-boot-script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# HOST_DIR = host dir
+# BOARD_DIR = board specific dir
+# BUILD_DIR = base dir/build
+# BINARIES_DIR = images dir
+# TARGET_DIR = target dir
+# RETROLX_BINARIES_DIR = batocera binaries sub directory
+
+HOST_DIR=$1
+BOARD_DIR=$2
+BUILD_DIR=$3
+BINARIES_DIR=$4
+TARGET_DIR=$5
+RETROLX_BINARIES_DIR=$6
+
+# ATF
+"${BR2_EXTERNAL_RETROLX_PATH}/board/batocera/scripts/build-atf.sh" "${HOST_DIR}" "${BOARD_DIR}" "${BINARIES_DIR}" gxl
+
+# U-Boot
+mkdir -p "${RETROLX_BINARIES_DIR}/build-uboot-khadas-vim2"     || exit 1
+cp "${BOARD_DIR}/build-uboot.sh"          "${RETROLX_BINARIES_DIR}/build-uboot-khadas-vim2/" || exit 1
+cd "${RETROLX_BINARIES_DIR}/build-uboot-khadas-vim2/" && ./build-uboot.sh "${HOST_DIR}" "${BOARD_DIR}" "${BINARIES_DIR}" || exit 1
+
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/packages" || exit 1
+cp -r "${BUILD_DIR}"/repo/* "${RETROLX_BINARIES_DIR}/boot/packages/" || exit 1
+
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/extlinux" || exit 1
+
+"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${RETROLX_BINARIES_DIR}/boot/boot/linux" || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${RETROLX_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+cp "${BINARIES_DIR}/initrd.gz"       "${RETROLX_BINARIES_DIR}/boot/boot/"                || exit 1
+cp "${BINARIES_DIR}/modules"         "${RETROLX_BINARIES_DIR}/boot/boot/modules"         || exit 1
+
+cp "${BOARD_DIR}/boot/boot-logo.bmp.gz" "${RETROLX_BINARIES_DIR}/boot/"          || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"    "${RETROLX_BINARIES_DIR}/boot/extlinux/" || exit 1
+cp "${BOARD_DIR}/boot/logo.bmp"         "${RETROLX_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BOARD_DIR}/boot/boot.cmd"         "${RETROLX_BINARIES_DIR}/boot/"          || exit 1
+
+for DTB in meson-gxm-khadas-vim2 meson-gxm-nexbox-a1 meson-gxm-q200 meson-gxm-q201 meson-gxm-rbox-pro meson-gxm-vega-s96 meson-gxm-s912-libretech-pc
+do
+	cp "${BINARIES_DIR}/${DTB}.dtb" "${RETROLX_BINARIES_DIR}/boot/boot/" || exit 1
+done
+
+exit 0
