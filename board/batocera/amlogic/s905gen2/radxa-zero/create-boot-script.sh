0d2addd3b39214390a62c6576568b6a43b896cc4
Romain Tisserand <romain.tisserand@gmail.com>
2021-11-14 22:12:05 +0100

    Add untested Raxda Zero support

--- board/batocera/amlogic/s905gen2/radxa-zero/create-boot-script.sh
+++ board/batocera/amlogic/s905gen2/radxa-zero/create-boot-script.sh
@@ -5,23 +5,34 @@
 # BUILD_DIR = base dir/build
 # BINARIES_DIR = images dir
 # TARGET_DIR = target dir
-# BATOCERA_BINARIES_DIR = batocera binaries sub directory
+# RETROLX_BINARIES_DIR = RetroLX binaries sub directory
 
 HOST_DIR=$1
 BOARD_DIR=$2
 BUILD_DIR=$3
 BINARIES_DIR=$4
 TARGET_DIR=$5
-BATOCERA_BINARIES_DIR=$6
+RETROLX_BINARIES_DIR=$6
 
-mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"     || exit 1
-mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
+# ATF
+"${BR2_EXTERNAL_RETROLX_PATH}/board/batocera/scripts/build-atf.sh" "${HOST_DIR}" "${BOARD_DIR}" "${BINARIES_DIR}" g12a
 
-cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
-cp "${BINARIES_DIR}/initrd.gz"       "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.gz"       || exit 1
-cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+# U-Boot
+mkdir -p "${RETROLX_BINARIES_DIR}/build-uboot-radxa-zero"     || exit 1
+cp "${BOARD_DIR}/build-uboot.sh"          "${RETROLX_BINARIES_DIR}/build-uboot-radxa-zero/" || exit 1
+cd "${RETROLX_BINARIES_DIR}/build-uboot-radxa-zero/" && ./build-uboot.sh "${HOST_DIR}" "${BOARD_DIR}" "${BINARIES_DIR}" || exit 1
 
-cp "${BINARIES_DIR}/meson-g12a-radxa-zero.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
-cp "${BOARD_DIR}/boot/extlinux.conf"                   "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/packages" || exit 1
+cp -r "${BUILD_DIR}"/repo/* "${RETROLX_BINARIES_DIR}/boot/packages/" || exit 1
+
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/extlinux" || exit 1
+
+cp "${BINARIES_DIR}/Image"           "${RETROLX_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"       "${RETROLX_BINARIES_DIR}/boot/boot/initrd.gz"       || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${RETROLX_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+
+cp "${BINARIES_DIR}/meson-g12a-radxa-zero.dtb" "${RETROLX_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"                   "${RETROLX_BINARIES_DIR}/boot/extlinux/" || exit 1
 
 exit 0
