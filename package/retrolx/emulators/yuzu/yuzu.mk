20f132fa35eaaba7120685ac2ac567a1a1f2de30
Romain Tisserand <romain.tisserand@gmail.com>
2021-11-15 12:52:25 +0100

    Bump and fix Yuzu

--- package/retrolx/emulators/yuzu/yuzu.mk
+++ package/retrolx/emulators/yuzu/yuzu.mk
@@ -3,8 +3,8 @@
 # YUZU
 #
 ################################################################################
-# Version.: Commits on Oct 12, 2021
-YUZU_VERSION = EA-2127
+# Version.: Commits on Nov 15, 2021
+YUZU_VERSION = EA-2201
 YUZU_SITE = https://github.com/pineappleEA/pineapple-src.git
 #YUZU_VERSION = mainline-0-775
 #YUZU_SITE = https://github.com/yuzu-emu/yuzu-mainline.git
@@ -12,6 +12,10 @@ YUZU_SITE_METHOD=git
 YUZU_GIT_SUBMODULES=YES
 YUZU_LICENSE = GPLv2
 YUZU_DEPENDENCIES = qt5base qt5tools qt5multimedia fmt boost ffmpeg zstd zlib libzip lz4 catch2 sdl2 opus
+
+ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
+YUZU_DEPENDENCIES += host-glslang
+endif
 
 # Should be set when the package cannot be built inside the source tree but needs a separate build directory.
 YUZU_SUPPORTS_IN_SOURCE_BUILD = NO
