e82c1afd306e2fd7402357155be79a1e0093d432
Romain TISSERAND <romain.tisserand@gmail.com>
2021-11-19 18:54:59 +0100

    Add support for orangepi-pc2 (Allwinner H5 board)

---
+++ board/retrolx/allwinner/h5/orangepi-pc2/build-uboot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+HOST_DIR=$1
+BOARD_DIR=$2
+IMAGES_DIR=$3
+
+# ARM Trusted Firmware BL31
+export BL31="${IMAGES_DIR}/atf/sun50i_a64/bl31.bin"
+
+# Crust firmware (optional)
+export SCP="/dev/null"
+
+# Clone U-Boot mainline
+wget "https://ftp.denx.de/pub/u-boot/u-boot-2021.10.tar.bz2"
+tar xf u-boot-2021.10.tar.bz2
+cd u-boot-2021.10
+
+# Apply patches
+PATCHES="${BOARD_DIR}/patches/uboot/*.patch"
+for patch in $PATCHES
+do
+  echo "Applying patch: $patch"
+  patch -p1 < $patch
+done
+
+# Make config
+make orangepi_pc2_defconfig
+
+# Build it
+ARCH=aarch64 CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-" make -j$(nproc)
+mkdir -p ../../uboot-orangepi-pc2
+
+# Copy to appropriate place
+cp u-boot-sunxi-with-spl.bin ../../uboot-orangepi-pc2/
