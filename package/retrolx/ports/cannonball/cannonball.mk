9d0e40ca89456489fb054c6599cd6b32bb504949
Romain TISSERAND <romain.tisserand@gmail.com>
2021-11-19 09:57:10 +0100

    Bump Cannonball, needs to be converted to package

--- package/retrolx/ports/cannonball/cannonball.mk
+++ package/retrolx/ports/cannonball/cannonball.mk
@@ -3,27 +3,21 @@
 # cannonball
 #
 ################################################################################
-# Version.: Commits on Jun 24, 2020
-CANNONBALL_VERSION = b6aa525ddd552f96b43b3b3a6f69326a277206bd
+# Version.: Commits on Nov 9, 2021
+CANNONBALL_VERSION = 62feed27e6976818348035c660d506a2aed97b40
 CANNONBALL_SITE = $(call github,djyt,cannonball,$(CANNONBALL_VERSION))
 CANNONBALL_LICENSE = GPLv2
 CANNONBALL_DEPENDENCIES = sdl2 boost
 
-CANNONBALL_TARGET = sdl2gles
-
-ifeq ($(BR2_PACKAGE_RETROLX_TARGET_RPI4),y)
-        CANNONBALL_TARGET = sdl2gles_rpi
-else ifeq ($(BR2_PACKAGE_RETROLX_TARGET_RPI3),y)
-        CANNONBALL_TARGET = sdl2gles_rpi
-else ifeq ($(BR2_PACKAGE_RETROLX_TARGET_X86),y)
-        CANNONBALL_TARGET = sdl2gl
-else ifeq ($(BR2_PACKAGE_RETROLX_TARGET_X86_64),y)
-        CANNONBALL_TARGET = sdl2gl
+ifeq ($(BR2_PACKAGE_RETROLX_TARGET_X86)$(BR2_PACKAGE_RETROLX_TARGET_X86_64),y)
+        CANNONBALL_TARGET = linux.cmake
+else
+        CANNONBALL_TARGET = linux-opengles.cmake
 endif
 
 # Build as release with proper target and paths
 CANNONBALL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DTARGET=$(CANNONBALL_TARGET)
-CANNONBALL_CONF_OPTS += -Droms_directory=/userdata/roms/cannonball/ -Dxml_directory=/userdata/system/configs/cannonball/ -Dres_directory=/userdata/system/configs/cannonball/
+#CANNONBALL_CONF_OPTS += -Droms_directory=/userdata/roms/cannonball/ -Dxml_directory=/userdata/system/configs/cannonball/ -Dres_directory=/userdata/system/configs/cannonball/
 
 
 # Cannonball cmake files are hopelessly broken.
@@ -36,26 +30,27 @@ endif
 # Enabling LTO as hires mode tends to be slow, it does help video rendering loops
 CANNONBALL_EXE_LINKER_FLAGS += -flto
 CANNONBALL_SHARED_LINKER_FLAGS += -flto
-CANNONBALL_CONF_OPTS += -DCMAKE_CXX_FLAGS=-flto -DCMAKE_EXE_LINKER_FLAGS="$(CANNONBALL_EXE_LINKER_FLAGS)" -DCMAKE_SHARED_LINKER_FLAGS="$(CANNONBALL_SHARED_LINKER_FLAGS)"
+CANNONBALL_CONF_OPTS += -DCMAKE_CXX_FLAGS="-O3 -flto=auto" -DCMAKE_EXE_LINKER_FLAGS="$(CANNONBALL_EXE_LINKER_FLAGS)" -DCMAKE_SHARED_LINKER_FLAGS="$(CANNONBALL_SHARED_LINKER_FLAGS)"
 
 # We need to build out-of-tree
 CANNONBALL_SUPPORTS_IN_SOURCE_BUILD = NO
 
 define CANNONBALL_SETUP_CMAKE
-	cp $(@D)/cmake/* $(@D)/
-	$(SED) "s+../res/config+\./res/config+g" $(@D)/CMakeLists.txt
-	$(SED) "s+../src/main+\./src/main+g" $(@D)/CMakeLists.txt
-	$(SED) "s+../res/tilemap.bin+\./res/tilemap.bin +g" $(@D)/CMakeLists.txt
-	$(SED) "s+../res/tilepatch.bin+\./res/tilepatch.bin +g" $(@D)/CMakeLists.txt
+	cp -r $(@D)/cmake/* $(@D)/
+	$(SED) "s+\.\./res/config.xml+\./res/config.xml+g" $(@D)/CMakeLists.txt
+	$(SED) "s+\.\./res/tilemap.bin+\./res/tilemap.bin+g" $(@D)/CMakeLists.txt
+	$(SED) "s+\.\./res/tilepatch.bin+\./res/tilepatch.bin+g" $(@D)/CMakeLists.txt
+	$(SED) "s+\.\./res/gamecontrollerdb.txt+\./res/gamecontrollerdb.txt+g" $(@D)/CMakeLists.txt
+	$(SED) "s+\.\./src/main+\./src/main+g" $(@D)/CMakeLists.txt
         $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/CMakeLists.txt
-        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2.cmake
-        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gl.cmake
-        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gles.cmake
-        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gles_rpi.cmake
+        #$(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2.cmake
+        #$(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gl.cmake
+        #$(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gles.cmake
+        #$(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gles_rpi.cmake
 
 	# Rpi4 64-bit compilation
-	$(SED) "s+-mfpu=neon-fp-armv8++g" $(@D)/sdl2gles_rpi.cmake
-	$(SED) "s+-mfloat-abi=hard++g" $(@D)/sdl2gles_rpi.cmake
+	#$(SED) "s+-mfpu=neon-fp-armv8++g" $(@D)/sdl2gles_rpi.cmake
+	#$(SED) "s+-mfloat-abi=hard++g" $(@D)/sdl2gles_rpi.cmake
 
         #$(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2.cmake
         #$(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2gl.cmake
