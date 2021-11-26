f13883fe11919b8376733c2a490d48fde65b9449
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-11-27 01:26:52 +0200

    Update firmware-wlan-aml

--- package/retrolx/firmware/firmware-wlan-aml/firmware-wlan-aml.mk
+++ package/retrolx/firmware/firmware-wlan-aml/firmware-wlan-aml.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FIRMWARE_WLAN_AML_VERSION = 3ddc301c272f081aa5513c1934f6d530bf80de4a
+FIRMWARE_WLAN_AML_VERSION = ee28faccfd65c17e1e6afbf103b6a81fa13fe815
 FIRMWARE_WLAN_AML_SITE = $(call github,libreelec,brcmfmac_sdio-firmware,$(FIRMWARE_WLAN_AML_VERSION))
 
 FIRMWARE_WLAN_AML_TARGET_DIR=$(TARGET_DIR)/lib/firmware/brcm
