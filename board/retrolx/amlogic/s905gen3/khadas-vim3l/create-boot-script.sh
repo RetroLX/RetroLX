4d29179971d17c78571a269516db90f27c714f18
Romain Tisserand <romain.tisserand@gmail.com>
2021-11-30 17:01:17 +0100

    arm64-a55-gles3 target

--- board/retrolx/amlogic/s905gen3/khadas-vim3l/create-boot-script.sh
+++ board/retrolx/amlogic/s905gen3/khadas-vim3l/create-boot-script.sh
@@ -29,14 +29,13 @@ cd "${RETROLX_BINARIES_DIR}/build-uboot-khadas-vim3l/" && ./build-uboot.sh "${HO
 mkdir -p "${RETROLX_BINARIES_DIR}/boot/boot"     || exit 1
 mkdir -p "${RETROLX_BINARIES_DIR}/boot/extlinux" || exit 1
 
-"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux -d "${BINARIES_DIR}/Image" "${RETROLX_BINARIES_DIR}/boot/boot/linux" || exit 1
+"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux -d "${BINARIES_DIR}/kernel-s905gen3/Image" "${RETROLX_BINARIES_DIR}/boot/boot/linux" || exit 1
 cp "${BINARIES_DIR}/initrd.gz"       "${RETROLX_BINARIES_DIR}/boot/boot/initrd.gz"       || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs" "${RETROLX_BINARIES_DIR}/boot/boot/retrolx.update" || exit 1
-cp "${BINARIES_DIR}/modules"         "${RETROLX_BINARIES_DIR}/boot/boot/modules"         || exit 1
+cp "${BINARIES_DIR}/kernel-s905gen3/modules"         "${RETROLX_BINARIES_DIR}/boot/boot/modules"         || exit 1
 
 cp "${BOARD_DIR}/boot/boot-logo.bmp.gz"                "${RETROLX_BINARIES_DIR}/boot/"          || exit 1
-cp "${BINARIES_DIR}/meson-sm1-khadas-vim3l.dtb"        "${RETROLX_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BINARIES_DIR}/kernel-s905gen3/meson-sm1-khadas-vim3l.dtb"        "${RETROLX_BINARIES_DIR}/boot/boot/"     || exit 1
 cp "${BOARD_DIR}/boot/extlinux.conf"                   "${RETROLX_BINARIES_DIR}/boot/extlinux/" || exit 1
-cp "${BOARD_DIR}/boot/extlinux.conf"                   "${RETROLX_BINARIES_DIR}/boot/boot/"     || exit 1
 
 exit 0
