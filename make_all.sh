279a98f7135d408cbab4c5d05205ab9eb1d0e03b
Romain Tisserand <romain.tisserand@gmail.com>
2021-12-03 11:45:39 +0100

    Update make_all.sh with multi-boards targets

--- make_all.sh
+++ make_all.sh
@@ -5,6 +5,13 @@ make arm64-a53-gles2-build
 make arm64-a53-gles3-build
 make arm64-a55-gles3-build
 
+# Allwinner
+make aw32-build
+
+# Amlogic
+make s812-build
+make s922x-build
+
 # Raspberry Pi
 make rpi1-build
 make rpi2-build
@@ -16,15 +23,8 @@ make rk3288-build
 make rk3326-build
 make rk3399-build
 
-# Amlogic
-make s812-build
-make s922x-build
-
 # Samsung
 make odroidxu4-build
-
-# Allwinner
-make aw32-build
 
 # x86
 make x86_64-build
