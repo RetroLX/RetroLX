6487bb6bffb06f50c2137e8f74bab830f86f7b93
Romain TISSERAND <romain.tisserand@gmail.com>
2021-12-13 17:31:16 +0100

    Bump kernel for allwinner 32-bit

--- package/retrolx/kernels/retrolx-kernel-aw32/kernel-aw32.mk
+++ package/retrolx/kernels/retrolx-kernel-aw32/kernel-aw32.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Mainline kernels
-RETROLX_KERNEL_AW32_VERSION = 5.15.6
+RETROLX_KERNEL_AW32_VERSION = 5.15.7
 RETROLX_KERNEL_AW32_ARCH = aw32
 RETROLX_KERNEL_AW32_SOURCE = kernel-$(RETROLX_KERNEL_AW32_ARCH)-$(RETROLX_KERNEL_AW32_VERSION).tar.gz
 RETROLX_KERNEL_AW32_SITE = https://repository.retrolx.org/kernel/$(RETROLX_KERNEL_AW32_ARCH)/$(RETROLX_KERNEL_AW32_VERSION)
