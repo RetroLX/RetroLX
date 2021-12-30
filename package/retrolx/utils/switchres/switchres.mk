bff59a48abf514e6c3a9cd3f7c067a079ed5e595
Romain Tisserand <romain.tisserand@gmail.com>
2021-12-31 00:55:28 +0100

    Bump switchres, install grid by default

--- package/retrolx/utils/switchres/switchres.mk
+++ package/retrolx/utils/switchres/switchres.mk
@@ -7,7 +7,12 @@
 SWITCHRES_VERSION = 5ea0efb278de119694a8128a6016821612422bde
 SWITCHRES_SITE = $(call github,antonioginer,switchres,$(SWITCHRES_VERSION))
 
-SWITCHRES_DEPENDENCIES = libdrm xserver_xorg-server
+SWITCHRES_DEPENDENCIES = libdrm
+
+ifeq ($(BR2_x86_64),y)
+SWITCHRES_DEPENDENCIES += xserver_xorg-server
+endif
+
 SWITCHRES_INSTALL_STAGING = YES
 
 define SWITCHRES_BUILD_CMDS
@@ -19,7 +24,7 @@ define SWITCHRES_BUILD_CMDS
         PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config --define-prefix" \
         PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
 	CPPFLAGS="-I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/SDL2 -I$(STAGING_DIR)/usr/include/drm -lSDL2" \
-	$(MAKE) PREFIX="$(STAGING_DIR)/usr" all
+	$(MAKE) PREFIX="$(STAGING_DIR)/usr" all grid
 endef
 
 #	LDFLAGS="-lSDL2 -ldrm"
@@ -38,6 +43,7 @@ endef
 define SWITCHRES_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/libswitchres.so $(TARGET_DIR)/usr/lib/libswitchres.so
 	$(INSTALL) -D -m 0755 $(@D)/switchres $(TARGET_DIR)/usr/bin/switchres
+	$(INSTALL) -D -m 0755 $(@D)/grid $(TARGET_DIR)/usr/bin/grid
 endef
 
 $(eval $(generic-package))
