a2b9a2763c2bb83930a3fe6b64ff97a33c8839c3
Romain Tisserand <romain.tisserand@gmail.com>
2021-12-31 01:27:11 +0100

    Rename batocera-gpicase to retrolx-gpicase

--- package/retrolx/utils/gpicase/gpicase.mk
+++ package/retrolx/utils/gpicase/gpicase.mk
@@ -12,7 +12,7 @@ define GPICASE_INSTALL_TARGET_CMDS
 	cp $(@D)/GPi_Case_patch/patch_files/overlays/dpi24.dtbo             $(BINARIES_DIR)/rpi-firmware/overlays/dpi24_gpicase.dtbo
 	cp $(@D)/GPi_Case_patch/patch_files/overlays/pwm-audio-pi-zero.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/pwm-audio-pi-zero_gpicase.dtbo
 	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/utils/gpicase/99-gpicase.rules                  $(TARGET_DIR)/etc/udev/rules.d
-	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/utils/gpicase/batocera-gpicase-install          $(TARGET_DIR)/usr/bin/batocera-gpicase-install
+	cp $(BR2_EXTERNAL_RETROLX_PATH)/package/retrolx/utils/gpicase/retrolx-gpicase-install          $(TARGET_DIR)/usr/bin/retrolx-gpicase-install
 endef
 
 define GPICASE_EXTRACT_CMDS
