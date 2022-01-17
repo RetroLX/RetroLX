d641cabacbd3a766653325c92439caf0c4d77294
Romain TISSERAND <romain.tisserand@gmail.com>
2022-01-17 14:15:25 +0100

    Fix tbb build with musl

--- package/retrolx/libraries/tbb/tbb.mk
+++ package/retrolx/libraries/tbb/tbb.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-TBB_VERSION = v2021.3.0
+TBB_VERSION = v2021.5.0
 TBB_SITE =  $(call github,oneapi-src,oneTBB,$(TBB_VERSION))
 TBB_INSTALL_STAGING = YES
 
-TBB_CONF_OPTS = -DTBB_TEST=OFF -DTBB_STRICT=OFF
+TBB_CONF_OPTS = -DTBB_TEST=OFF -DTBB_STRICT=OFF -DTBBMALLOC_BUILD=OFF
 
 ifeq ($(BR2_arm),y)
 TBB_CONF_OPTS += -DCMAKE_C_FLAGS=-D__aarch32__ -DCMAKE_CXX_FLAGS=-D__aarch32__
