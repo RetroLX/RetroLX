9e9511f48d4d6a90cdc6473d988b457db3318a47
Romain TISSERAND <romain.tisserand@gmail.com>
2022-01-31 15:00:06 +0100

    BananaPi M2 Zero

---
+++ board/retrolx/allwinner/aw32/bananapi-m2-zero/build-uboot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+HOST_DIR=$1
+IMAGES_DIR=$2
+
+# Clone U-Boot mainline
+wget "https://ftp.denx.de/pub/u-boot/u-boot-2022.01.tar.bz2"
+tar xf u-boot-2022.01.tar.bz2
+cd u-boot-2022.01
+
+# Apply patches
+PATCHES="${BOARD_DIR}/patches/uboot/*.patch"
+for patch in $PATCHES
+do
+  echo "Applying patch: $patch"
+  patch -p1 < $patch
+done
+
+# Make config
+make bananapi_m2_zero_defconfig
+
+# Build it
+ARCH=arm CROSS_COMPILE="${HOST_DIR}/bin/arm-buildroot-linux-gnueabihf-" make -j$(nproc)
+mkdir -p ../../uboot-cha
+
+# Copy to appropriate place
+cp u-boot-sunxi-with-spl.bin ../../uboot-bananapi-m2-zero/
---
+++ board/retrolx/allwinner/aw32/bananapi-m2-zero/create-boot-script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# HOST_DIR = host dir
+# BOARD_DIR = board specific dir
+# BUILD_DIR = base dir/build
+# BINARIES_DIR = images dir
+# TARGET_DIR = target dir
+# RETROLX_BINARIES_DIR = RetroLX binaries sub directory
+
+HOST_DIR=$1
+BOARD_DIR=$2
+BUILD_DIR=$3
+BINARIES_DIR=$4
+TARGET_DIR=$5
+RETROLX_BINARIES_DIR=$6
+
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/packages" || exit 1
+cp "${BUILD_DIR}/board/retrolx/allwinner/aw32/bananapi-m2-zero/packages.txt" "${RETROLX_BINARIES_DIR}/boot/packages.txt"
+cat "${RETROLX_BINARIES_DIR}/boot/packages.txt" | while read line; do cp "${BUILD_DIR}/repo/${line}"* "${RETROLX_BINARIES_DIR}/boot/packages/"; done
+
+mkdir -p "${RETROLX_BINARIES_DIR}/uboot"     || exit 1
+cp "${BOARD_DIR}/build-uboot.sh"          "${RETROLX_BINARIES_DIR}/uboot/" || exit 1
+cd "${RETROLX_BINARIES_DIR}/uboot/" && ./build-uboot.sh "${HOST_DIR}" "${BINARIES_DIR}" || exit 1
+
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${RETROLX_BINARIES_DIR}/boot/extlinux" || exit 1
+
+cp "${BINARIES_DIR}/zImage"           "${RETROLX_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"       "${RETROLX_BINARIES_DIR}/boot/boot/initrd.gz"       || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${RETROLX_BINARIES_DIR}/boot/boot/retrolx.update" || exit 1
+cp "${BINARIES_DIR}/modules"         "${RETROLX_BINARIES_DIR}/boot/boot/modules"         || exit 1
+
+cp "${BINARIES_DIR}/sun8i-h2-plus-bananapi-m2-zero.dtb" "${RETROLX_BINARIES_DIR}/boot/boot/sun8i-h2-plus-bananapi-m2-zero.dtb"     || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"          "${RETROLX_BINARIES_DIR}/boot/extlinux/" || exit 1
+
+exit 0
