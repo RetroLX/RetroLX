aeb1e3eb16a826cff32bf5b54b25dfe0df9c638c
Romain TISSERAND <romain.tisserand@gmail.com>
2022-02-01 11:51:09 +0100

    Bump Dosbox-X

--- package/retrolx/emulators/dosbox-x/dosbox-x.mk
+++ package/retrolx/emulators/dosbox-x/dosbox-x.mk
@@ -3,8 +3,8 @@
 # DosBox-X
 #
 ################################################################################
-# Version.: Commits on Jan 1, 2022
-DOSBOX_X_VERSION = dosbox-x-v0.83.21
+# Version.: Commits on Feb 1, 2022
+DOSBOX_X_VERSION = dosbox-x-v0.83.22
 DOSBOX_X_SITE = $(call github,joncampbell123,dosbox-x,$(DOSBOX_X_VERSION))
 DOSBOX_X_DEPENDENCIES = sdl2 sdl2_net sdl_sound zlib libpng libogg libvorbis ffmpeg
 DOSBOX_X_LICENSE = GPLv2
