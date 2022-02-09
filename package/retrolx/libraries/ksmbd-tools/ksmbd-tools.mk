cb6bfd534fd042f24affdb87975675e50e362307
Romain TISSERAND <romain.tisserand@gmail.com>
2022-02-09 13:16:50 +0100

    remove closed stuff useless + bump ksmbd tools

--- package/retrolx/libraries/ksmbd-tools/ksmbd-tools.mk
+++ package/retrolx/libraries/ksmbd-tools/ksmbd-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KSMBD_TOOLS_VERSION = 2f8cfd97fd1f494f651a1a40db1be54138d558a5
+KSMBD_TOOLS_VERSION = a47af62bc4c7e8e486b318347cee03e40b0ea40b
 KSMBD_TOOLS_SITE =  $(call github,namjaejeon,ksmbd-tools,$(KSMBD_TOOLS_VERSION))
 KSMBD_TOOLS_INSTALL_STAGING = YES
 KSMBD_TOOLS_AUTORECONF = YES
