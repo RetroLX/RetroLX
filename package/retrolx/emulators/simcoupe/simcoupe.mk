e1d772b8d7df5ce4f982944e61b4cf155d64388f
Romain TISSERAND <romain.tisserand@gmail.com>
2022-03-13 16:38:35 +0100

    Bump Simcoupe to v1.2.9

--- package/retrolx/emulators/simcoupe/simcoupe.mk
+++ package/retrolx/emulators/simcoupe/simcoupe.mk
@@ -3,8 +3,8 @@
 # SIMCOUPE
 #
 ################################################################################
-# Version.: Release on Nov 8, 2021
-SIMCOUPE_VERSION = v1.2.8
+# Version.: Release on Mar 13, 2021
+SIMCOUPE_VERSION = v1.2.9
 SIMCOUPE_SITE = $(call github,simonowen,simcoupe,$(SIMCOUPE_VERSION))
 SIMCOUPE_LICENSE = GPLv2
 SIMCOUPE_DEPENDENCIES = sdl2 zlib libpng
