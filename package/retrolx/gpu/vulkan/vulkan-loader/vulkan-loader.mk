5a2d9a0ad121ddcb83137f1496d2b3f4687a0241
Romain Tisserand <romain.tisserand@gmail.com>
2022-03-25 18:25:21 +0100

    Bump Vulkan to latest v1.2.y

--- package/retrolx/gpu/vulkan/vulkan-loader/vulkan-loader.mk
+++ package/retrolx/gpu/vulkan/vulkan-loader/vulkan-loader.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VULKAN_LOADER_VERSION = v1.2.199
+VULKAN_LOADER_VERSION = v1.2.203
 
 VULKAN_LOADER_SITE =  $(call github,KhronosGroup,Vulkan-Loader,$(VULKAN_LOADER_VERSION))
 VULKAN_LOADER_DEPENDENCIES = vulkan-headers
