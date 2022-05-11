71e1bad5bf98cbe4c84c02f6916bbbc20f3a32d0
Romain Tisserand <romain.tisserand@gmail.com>
2022-05-11 21:51:22 +0200

    Bump linux-firmware to 20220509

--- package/retrolx/firmware/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/retrolx/firmware/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -3,8 +3,8 @@
 # alllinuxfirmwares
 #
 ################################################################################
-# Version from 2022-02-09
-ALLLINUXFIRMWARES_VERSION = 20220209
+# Version from 2022-05-09
+ALLLINUXFIRMWARES_VERSION = 20220509
 ALLLINUXFIRMWARES_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 ALLLINUXFIRMWARES_SITE_METHOD = git
 
