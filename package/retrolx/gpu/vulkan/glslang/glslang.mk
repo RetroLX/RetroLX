f97ec74a74d1cee9014ab9bdc07ac7378e3d9fbe
Romain Tisserand <romain.tisserand@gmail.com>
2022-05-12 16:48:38 +0200

    Bump/proper glslang

--- package/retrolx/gpu/vulkan/glslang/glslang.mk
+++ package/retrolx/gpu/vulkan/glslang/glslang.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GLSLANG_VERSION = d1608ab1ef17f1488bdcbfe11f2c3c96ac482fce
+GLSLANG_VERSION = 11.9.0
 GLSLANG_SITE =  https://github.com/KhronosGroup/glslang
 GLSLANG_SITE_METHOD=git
 GLSLANG_DEPENDENCIES = vulkan-headers vulkan-loader
