57e5e8e4e11aab785a64eb93eeb994201bb8147f
Romain TISSERAND <romain.tisserand@gmail.com>
2022-06-26 08:05:55 +0200

    bump lr dosbox pure

--- package/retrolx/emulators/libretro/libretro-dosbox-pure/libretro-dosbox-pure.mk
+++ package/retrolx/emulators/libretro/libretro-dosbox-pure/libretro-dosbox-pure.mk
@@ -3,8 +3,8 @@
 # DOSBOX PURE
 #
 ################################################################################
-# Version.: Commits on Jun 23, 2022
-LIBRETRO_DOSBOX_PURE_VERSION = 0.9.5
+# Version.: Commits on Jun 26, 2022
+LIBRETRO_DOSBOX_PURE_VERSION = 0.9.6
 LIBRETRO_DOSBOX_PURE_SITE = $(call github,schellingb,dosbox-pure,$(LIBRETRO_DOSBOX_PURE_VERSION))
 LIBRETRO_DOSBOX_PURE_LICENSE = GPLv2
 
