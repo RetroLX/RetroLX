ddb165cd8d24f13d08406732574a95cda80f71e1
Romain TISSERAND <romain.tisserand@gmail.com>
2022-08-02 23:39:22 +0200

    Bump fsuae

--- package/retrolx/emulators/fsuae/fsuae.mk
+++ package/retrolx/emulators/fsuae/fsuae.mk
@@ -3,13 +3,19 @@
 # FSUAE
 #
 ################################################################################
-# Version.: Commits on Oct 21, 2021 (4.0.x)
-FSUAE_VERSION = f72c9c9dd149aae7090640dc2c0ba2e746ca2e3e
+# Version.: Commits on Jul 28, 2022 (4.0.x)
+FSUAE_VERSION = bfa0c7522c6c5f73cceb340d677491d056febd01
 FSUAE_SITE = $(call github,FrodeSolheim,fs-uae,$(FSUAE_VERSION))
 FSUAE_LICENSE = GPLv2
-FSUAE_DEPENDENCIES = xserver_xorg-server openal libpng sdl2 zlib libmpeg2 libglib2 libcapsimage
+FSUAE_DEPENDENCIES = openal libpng sdl2 zlib libmpeg2 libglib2 libcapsimage
 
 FSUAE_CONF_OPTS += --disable-codegen
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+FSUAE_DEPENDENCIES += xserver_xorg-server
+else
+FSUAE_CONF_OPTS += --without-x11
+endif
 
 define FSUAE_HOOK_BOOTSTRAP
   cd $(@D) && ./bootstrap
