388fd41d39e259ed8fc94a1ecca5e31dbab7a5b6
Romain TISSERAND <romain.tisserand@gmail.com>
2022-08-02 11:36:39 +0200

    Bump appstream glib

--- package/retrolx/libraries/appstream-glib/appstream-glib.mk
+++ package/retrolx/libraries/appstream-glib/appstream-glib.mk
@@ -3,7 +3,7 @@
 # mali-g31-gbm
 #
 ################################################################################
-APPSTREAM_GLIB_VERSION = appstream_glib_0_7_18
+APPSTREAM_GLIB_VERSION = appstream_glib_0_8_0
 APPSTREAM_GLIB_SITE = $(call github,hughsie,appstream-glib,$(APPSTREAM_GLIB_VERSION))
 APPSTREAM_GLIB_INSTALL_STAGING = YES
 
