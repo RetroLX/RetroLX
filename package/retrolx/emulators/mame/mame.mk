885e056182cdfdfc2af565394c6fca7db2820c0d
Romain TISSERAND <romain.tisserand@gmail.com>
2022-08-02 15:51:03 +0200

    WIP mame0246

--- package/retrolx/emulators/mame/mame.mk
+++ package/retrolx/emulators/mame/mame.mk
@@ -3,8 +3,8 @@
 # MAME
 #
 ################################################################################
-# Version.: Release 0.243
-MAME_VERSION = mame0243
+# Version.: Release 0.246
+MAME_VERSION = mame0246
 MAME_SITE = $(call github,mamedev,mame,$(MAME_VERSION))
 MAME_DEPENDENCIES = sdl2 sdl2_ttf zlib libpng fontconfig sqlite jpeg flac rapidjson expat glm
 MAME_LICENSE = MAME
