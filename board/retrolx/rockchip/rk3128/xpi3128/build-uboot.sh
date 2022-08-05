96d1f657f73e3e7986ddea08974257a2d17fed3e
Romain TISSERAND <romain.tisserand@gmail.com>
2022-08-05 15:35:37 +0200

    Some RK3128 fixes

--- board/retrolx/rockchip/rk3128/xpi3128/build-uboot.sh
+++ board/retrolx/rockchip/rk3128/xpi3128/build-uboot.sh
@@ -4,7 +4,7 @@ HOST_DIR=$1
 IMAGES_DIR=$2
 
 # Clone U-Boot mainline
-wget "https://ftp.denx.de/pub/u-boot/u-boot-2022.07.tar.bz2"
+[ ! -f "u-boot-2022.07.tar.bz2" ] && wget "https://ftp.denx.de/pub/u-boot/u-boot-2022.07.tar.bz2"
 tar xf u-boot-2022.07.tar.bz2
 cd u-boot-2022.07
 
@@ -15,6 +15,9 @@ do
   echo "Applying patch: $patch"
   patch -p1 < $patch
 done
+
+# Make clean
+make clean
 
 # Make config
 make evb-rk3128_defconfig
@@ -27,9 +30,9 @@ mkdir -p ../../uboot-xpi3128
 git clone https://github.com/rockchip-linux/rkbin.git
 cd rkbin
 "${HOST_DIR}/bin/mkimage" -n rk3128 -T rksd -d "bin/rk31/rk3128_ddr_300MHz_v2.12.bin" "../idbloader.img" || exit 1
-cat "bin/rk31/rk3128x_miniloader_v2.57.bin" >> "../idbloader.img"
+cat "bin/rk31/rk312x_miniloader_v2.63.bin" >> "../idbloader.img"
 ./tools/loaderimage --pack --uboot "../u-boot-dtb.bin" "../u-boot.img" --size 1024 4
-./tools/loaderimage --pack --trustos "bin/rk31/rk3126_tee_ta_v2.01.bin" "../trust.img" --size 1024 4
+./tools/loaderimage --pack --trustos "bin/rk31/rk3126_tee_ta_v2.03.bin" "../trust.img" --size 1024 4
 cd ..
 
 # Copy to appropriate place
